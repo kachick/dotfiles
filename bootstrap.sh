0adbc6f3eacda5dbc35e08485404c6c06e8acfaa
Kenichi Kamiya <kachick1@gmail.com>
2022-07-29 22:43:47 +0900

    Install latest ruby only to make faster setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -86,9 +86,9 @@ install_asdf_plugins() {
 
 install_asdf_managed_tools() {
   # When faced ruby with OpenSSL issues, look at https://github.com/kachick/times_kachick/issues/180
-  RUBY_CONFIGURE_OPTS=--with-openssl-dir="$(brew --prefix openssl@3)" asdf install ruby 3.1.2
-  RUBY_CONFIGURE_OPTS=--with-openssl-dir="$(brew --prefix openssl@1.1)" asdf install ruby 3.0.4
-  RUBY_CONFIGURE_OPTS=--with-openssl-dir="$(brew --prefix openssl@1.1)" asdf install ruby 2.7.6
+  RUBY_CONFIGURE_OPTS=--with-openssl-dir="$(brew --prefix openssl@3)" asdf install ruby latest
+  # RUBY_CONFIGURE_OPTS=--with-openssl-dir="$(brew --prefix openssl@1.1)" asdf install ruby 3.0.4
+  # RUBY_CONFIGURE_OPTS=--with-openssl-dir="$(brew --prefix openssl@1.1)" asdf install ruby 2.7.6
 
   asdf install
 }
