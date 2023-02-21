2470f0bd0e49480764aa31ca0dc9c329a79917f3
Kenichi Kamiya <kachick1@gmail.com>
2023-02-21 15:24:54 +0900

    Needed this?

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
     nixpkgs = { url = "https://github.com/NixOS/nixpkgs/archive/8eabb500ebf5dcb9a40024c1150ef348cefa8320.tar.gz"; };
   };
 
+
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux; {
+    packages.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux;
+
     devShell.x86_64-linux = mkShell {
       buildInputs = [
         dprint
