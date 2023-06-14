3e537bd3b7db5ec3518e86275f00a1ab7cc10ac4
Kenichi Kamiya <kachick1@gmail.com>
2023-06-14 23:39:22 +0900

    Focus only for OSS major defauls

--- home/.aliases.sh
+++ home/.aliases.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-alias git-switch-default='git checkout develop 2>/dev/null || git checkout development 2>/dev/null || git checkout main 2>/dev/null || git checkout trunk 2>/dev/null || git checkout master 2>/dev/null'
+alias git-switch-default='git checkout main 2>/dev/null || git checkout master 2>/dev/null'
 alias git-remote-upsteram="git remote | grep -E '^upstream$'|| git remote | grep -E '^origin$'"
 # https://github.com/kyanny/git-delete-merged-branches/pull/6
-alias git-delete-merged-branches="git branch --merged | grep -vE '((^\*)|^ *(main|master|develop|development|trunk)$)' | xargs -I % git branch -d %"
+alias git-delete-merged-branches="git branch --merged | grep -vE '((^\*)|^ *(main|master)$)' | xargs -I % git branch -d %"
 alias git-cleanup-branches='git-switch-default && git pull $(git-remote-upsteram) $(git-current-branch) && git fetch $(git-remote-upsteram) --tags --prune && git-delete-merged-branches'
 
 alias la='exa --long --all --group-directories-first'
