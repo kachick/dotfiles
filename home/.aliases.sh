14eb837c973fc76ee4aa4ca7a28b21ef6d015284
Kenichi Kamiya <kachick1@gmail.com>
2023-06-15 13:10:09 +0900

    Follow git aliased alias

--- home/.aliases.sh
+++ home/.aliases.sh
@@ -4,7 +4,7 @@ alias git-switch-default='git checkout main 2>/dev/null || git checkout master 2
 alias git-remote-upsteram="git remote | grep -E '^upstream$'|| git remote | grep -E '^origin$'"
 # https://github.com/kyanny/git-delete-merged-branches/pull/6
 alias git-delete-merged-branches="git branch --merged | grep -vE '((^\*)|^ *(main|master)$)' | xargs -I % git branch -d %"
-alias git-cleanup-branches='git-switch-default && git pull $(git-remote-upsteram) $(git-current-branch) && git fetch $(git-remote-upsteram) --tags --prune && git-delete-merged-branches'
+alias git-cleanup-branches='git-switch-default && git pull $(git-remote-upsteram) $(git current-branch) && git fetch $(git-remote-upsteram) --tags --prune && git-delete-merged-branches'
 
 alias la='exa --long --all --group-directories-first'
 
