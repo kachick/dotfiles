0ac138187a21b138fd2c96d2eb52d0f41f94a00f
Kenichi Kamiya <kachick1@gmail.com>
2023-07-09 14:56:24 +0900

    Allow nodejs-16 for GitHub Action engine

---
+++ .config/nixpkgs/config.nix
@@ -0,0 +1,8 @@
+{
+  permittedInsecurePackages = [
+    # Because it is the only choice in the GitHub Actions engine.
+    # https://github.com/kachick/wait-other-jobs/blob/6a50464dd0f6a3cbde8ac50687ee246830f99075/CONTRIBUTING.md#why-using-nodejs16-instead-of-denobunnodejs18
+    # NOTE: Cant't specify as `nodejs-16_x`
+    "nodejs-16.20.0"
+  ];
+}
