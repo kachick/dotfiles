df1f34bea4d4cf608908fa3937dc3eb01b566c75
Kenichi Kamiya <kachick1@gmail.com>
2023-07-13 22:56:41 +0900

    Allow nodejs16 latest

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -4,5 +4,6 @@
     # https://github.com/kachick/wait-other-jobs/blob/6a50464dd0f6a3cbde8ac50687ee246830f99075/CONTRIBUTING.md#why-using-nodejs16-instead-of-denobunnodejs18
     # NOTE: Cant't specify as `nodejs-16_x`
     "nodejs-16.20.0"
+    "nodejs-16.20.1"
   ];
 }
