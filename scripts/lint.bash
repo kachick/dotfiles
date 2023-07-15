5504ec35a29242c44dffef65728f4ae61fa7c3c0
Kenichi Kamiya <kachick1@gmail.com>
2023-07-15 18:14:57 +0900

    Extract basic development tasks

---
+++ scripts/lint.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+shopt -s globstar
+
+dprint check
+shfmt --diff ./**/*.bash
+shellcheck ./**/*.bash
+nixpkgs-fmt --check ./**/*.nix
+typos . .github .config .vscode
+gitleaks detect
