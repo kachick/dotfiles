a9401b02f5267cbfad4fb19502531c94f4ae9cca
Kenichi Kamiya <kachick1@gmail.com>
2023-07-15 20:55:52 +0900

    Enable generiLinux

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -20,6 +20,10 @@
   # changes in each release.
   home.stateVersion = "23.05";
 
+  # This also changes xdg? Official manual sed this config is better for non NixOS Linux
+  # https://github.com/nix-community/home-manager/blob/559856748982588a9eda6bfb668450ebcf006ccd/modules/targets/generic-linux.nix#L16
+  targets.genericLinux.enable = true;
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
