0fc574ed73181ceaccaac3349664b206d1dc4831
Kenichi Kamiya <kachick1@gmail.com>
2023-07-28 18:18:59 +0900

    Remove unused scripts to put nix.conf
    Follow #192

--- scripts/enable_nix-experimental-features.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-mkdir -p "$XDG_CONFIG_HOME/nix"
-touch "$XDG_CONFIG_HOME/nix/nix.conf"
-rg '^experimental-features' "$XDG_CONFIG_HOME/nix/nix.conf" ||
-  echo 'experimental-features = nix-command flakes' >>"$XDG_CONFIG_HOME/nix/nix.conf"
