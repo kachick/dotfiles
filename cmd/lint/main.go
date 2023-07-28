3dc724d0e80b394e5ba405c9cdca528d3e007144
Kenichi Kamiya <kachick1@gmail.com>
2023-07-28 18:02:19 +0900

    Rewrite linter glue bash with go

---
+++ cmd/lint/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	doublestar "github.com/bmatcuk/doublestar/v4"
+)
+
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fsys := os.DirFS(wd)
+
+	bashPaths, err := doublestar.Glob(fsys, "./**/*.bash")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	nixPaths, err := doublestar.Glob(fsys, "./**/*.nix")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmds := []struct {
+		path string
+		args []string
+	}{
+		{"dprint", []string{"check"}},
+		{"shfmt", append([]string{"--diff"}, bashPaths...)},
+		{"shellcheck", bashPaths},
+		{"nixpkgs-fmt", append([]string{"--check"}, nixPaths...)},
+		{"typos", []string{".", ".github", ".config", ".vscode"}},
+		{"gitleaks", []string{"detect"}},
+		{"go", []string{"vet", "./..."}},
+	}
+
+	for _, cmd := range cmds {
+		output, err := exec.Command(cmd.path, cmd.args...).Output()
+		log.Printf("%s %s\n%s\n", cmd.path, strings.Join(cmd.args, " "), output)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kachick/dotfiles
 
 go 1.20
+
+require github.com/bmatcuk/doublestar/v4 v4.6.0
