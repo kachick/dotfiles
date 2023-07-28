e6fd1c4bb254466810ab92dfc39146a908df77bd
Kenichi Kamiya <kachick1@gmail.com>
2023-07-28 19:03:17 +0900

    Replace fmt glue bash with go

---
+++ cmd/fmt/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	doublestar "github.com/bmatcuk/doublestar/v4"
+)
+
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fsys := os.DirFS(wd)
+
+	bashPaths, err := doublestar.Glob(fsys, "./**/*.bash")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	nixPaths, err := doublestar.Glob(fsys, "./**/*.nix")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmds := []struct {
+		path string
+		args []string
+	}{
+		{"dprint", []string{"fmt"}},
+		{"shfmt", append([]string{"--write"}, bashPaths...)},
+		{"nixpkgs-fmt", nixPaths},
+		{"typos", []string{".", ".github", ".config", ".vscode", "--write-changes"}},
+		{"go", []string{"fmt", "./..."}},
+	}
+
+	for _, cmd := range cmds {
+		output, err := exec.Command(cmd.path, cmd.args...).Output()
+		log.Printf("%s %s\n%s\n", cmd.path, strings.Join(cmd.args, " "), output)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
