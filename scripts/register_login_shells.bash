28ca7eda7c72b26597bd87a2fbeb335ce24ef9c5
Kenichi Kamiya <kachick1@gmail.com>
2023-07-30 23:26:43 +0900

    Drop nushell from first place for now

--- scripts/register_login_shells.bash
+++ scripts/register_login_shells.bash
@@ -6,14 +6,12 @@ set -euo pipefail
 register_login_shells() {
 	local -r zsh_path="$(which zsh)"
 	local -r fish_path="$(which fish)"
-	local -r nushell_path="$(which nu)"
 
 	# https://stackoverflow.com/a/3557165/1212807
 	# https://stackoverflow.com/a/49049781/1212807
 	(
 		grep -qxF "$zsh_path" /etc/shells || echo "$zsh_path"
 		grep -qxF "$fish_path" /etc/shells || echo "$fish_path"
-		grep -qxF "$nushell_path" /etc/shells || echo "$nushell_path"
 	) | sudo tee -a /etc/shells
 }
 
