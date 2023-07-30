be884e2ffa49d99e0c3dbfe5a79a4cd97fad3c5a
Kenichi Kamiya <kachick1@gmail.com>
2023-07-30 13:21:00 +0900

    Fix commit miss

--- home/.config/homemade/.aliases.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-alias la='exa --long --all --group-directories-first'
-
-alias zj='zellij attach "$(basename "$PWD")" || zellij --session "$(basename "$PWD")"'
--- home/.config/homemade/bench_shells.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Keep under 120ms for main shells...!
-bench_shells() {
-	hyperfine 'zsh -i -c exit'
-	# Really having same options as zsh...?
-	hyperfine 'bash -i -c exit'
-	hyperfine 'nu -i -c exit'
-}
