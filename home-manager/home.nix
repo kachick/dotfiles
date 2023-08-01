0edd0716aceb1508544ddcac72d23a03d91c74b5
Kenichi Kamiya <kachick1@gmail.com>
2023-08-01 12:21:23 +0900

    Improve comment

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -90,7 +90,9 @@
     # Replacement of `programs.direnv.enableNixDirenvIntegration = true;`
     #
     # Make much faster, but I may add nix_direnv_watch_file in several repositories when it has `.ruby-version`
-    # See https://github.com/nix-community/nix-direnv/blob/ed2cb75553b4864e3c931a48e3a2cd43b93152c5/README.md#L368-L372
+    # See following reference
+    #   - https://github.com/nix-community/nix-direnv/blob/ed2cb75553b4864e3c931a48e3a2cd43b93152c5/README.md?plain=1#L368-L373
+    #   - https://github.com/kachick/ruby-ulid/pull/410
     nix-direnv = {
       enable = true;
     };
