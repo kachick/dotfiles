22d4ed67d3eac7c60dcbc697f0eba6977f6f2583
Kenichi Kamiya <kachick1@gmail.com>
2023-08-02 20:45:35 +0900

    Fix wrong? condition, and dynamic import of color schema

--- home-manager/darwin.nix
+++ home-manager/darwin.nix
@@ -1,5 +1,18 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
+# condition branch as `if pkgs.stdenv.hostPlatform.isDarwin then` will not work, but having the config even in Linux okay for me.
 {
   xdg.configFile."iterm2/com.googlecode.iterm2.plist".source = ../home/.config/iterm2/com.googlecode.iterm2.plist;
+
+  # Just putting the refererenced file to easy import, applying should be done via GUI and saving to plist
+  xdg.configFile."iterm2/OneHalfDark.itermcolors".text = builtins.readFile (
+    pkgs.fetchFromGitHub
+      {
+        owner = "mbadolato";
+        repo = "iTerm2-Color-Schemes";
+        rev = "3f8a0791ed9a99c10054026c1a8285459117e0f2";
+        sha256 = lib.fakeSha256;
+      }
+    + "/schemes/OneHalfDark.itermcolors"
+  );
 }
