5aa0e35fd584a968398b3a4e2652afc9f2e8c48a
Kenichi Kamiya <kachick1@gmail.com>
2023-08-04 21:52:00 +0900

    Install iTerm2 with home-manager

--- home-manager/darwin.nix
+++ home-manager/darwin.nix
@@ -7,22 +7,13 @@
 #      https://github.com/LnL7/nix-darwin/blob/16c07487ac9bc59f58b121d13160c67befa3342e/modules/system/defaults/finder.nix#L8-L14
 
 # https://github.com/nix-community/home-manager/issues/414#issuecomment-427163925
-let
-  iterm2Repository = pkgs.fetchFromGitHub
-    {
-      owner = "gnachman";
-      repo = "iTerm2";
-      rev = "e7c4c4b1ba6b21a19a48be2dad67048099be176e";
-      sha256 = "sha256-7F8l2QEnTMJlOCpT2WQ8f7iv8I96fMqDa5MM4oQAvYQ=";
-    };
-in
 lib.mkMerge [
   (lib.mkIf pkgs.stdenv.isDarwin {
     xdg.configFile."iterm2/com.googlecode.iterm2.plist".source = ../home/.config/iterm2/com.googlecode.iterm2.plist;
 
     # Do not use `programs.zsh.dotDir`, it does not refer xdg module
     xdg.configFile."zsh/.zshrc.darwin".text = ''
-      source ${iterm2Repository + "/Resources/shell_integration/iterm2_shell_integration.zsh"}
+      source ${pkgs.iterm2 + "/Applications/iTerm2.app/Contents/Resources/iterm2_shell_integration.zsh"}
     '';
 
     # Just putting the refererenced file to easy import, applying should be done via GUI and saving to plist
