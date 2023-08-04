ad980c23662a93cedb611a8563bf7c80599611a2
Kenichi Kamiya <kachick1@gmail.com>
2023-08-04 22:07:21 +0900

    Install iTerm2 with home-manager (#250)
    * Install iTerm2 with home-manager
    
    * Fix a typo and comments

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -9,6 +9,12 @@
 #     { };
 # in
 
+# FAQ
+#
+# A. How to know and get the paths inside a pkg?
+# Q. `nix path-info` is a way, installing iTerm2 shell integration used it. Access /nix/store~ path, and `ls` helps you.
+
+
 {
   home.packages = with pkgs; [
     # Use `bashInteractive`, don't `bash` - https://github.com/NixOS/nixpkgs/issues/29960, https://github.com/NixOS/nix/issues/730
@@ -95,6 +101,11 @@
     [
       # Fix missing locales as `locale: Cannot set LC_CTYPE to default locale`
       glibc
+    ]
+  ) ++ (lib.optionals stdenv.isDarwin
+    [
+      # https://github.com/NixOS/nixpkgs/commit/3ea22dab7d906f400cc5983874dbadeb8127c662#diff-32e42fa095503d211e9c2894de26c22166cafb875d0a366701922aa23976c53fL21-L33
+      iterm2
     ]
   );
 }
