3cd9350375720ab58afd986d19113f9223b73b06
Kenichi Kamiya <kachick1@gmail.com>
2023-08-10 03:41:08 +0900

    Enable IdentitiesOnly in ssh

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -70,16 +70,19 @@ in
     matchBlocks = {
       "github.com" = {
         identityFile = "${sshDir}/id_ed25519";
+        identitiesOnly = true;
         user = "git";
       };
 
       "gitlab.com" = {
         identityFile = "${sshDir}/id_ed25519";
+        identitiesOnly = true;
         user = "git";
       };
 
       "bitbucket.org" = {
         identityFile = "${sshDir}/id_ed25519";
+        identitiesOnly = true;
         user = "git";
       };
     };
