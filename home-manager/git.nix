5817fe7cb357359b169d0c023223f27b33d0dd80
Kenichi Kamiya <kachick1@gmail.com>
2023-08-10 00:57:17 +0900

    WIP

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,9 +1,59 @@
-{ ... }:
+{ config, ... }:
 
-# https://github.com/nix-community/home-manager/blob/master/modules/programs/lazygit.nix
 {
-  xdg.configFile."git/config".source = ../home/.config/git/config;
+  # https://github.com/nix-community/home-manager/blob/master/modules/programs/git.nix
+  # xdg will be used in home-manager: https://github.com/nix-community/home-manager/blob/7b8d43fbaf8450c30caaed5eab876897d0af891b/modules/programs/git.nix#L417-L418
+  programs.git = {
+    enable = true;
 
+    extraConfig = {
+      user = {
+        email = "kachick1@gmail.com";
+        name = "Kenichi Kamiya";
+        # TODO: Share code to get the path with ./ssh.nix
+        signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+      };
+
+      core = {
+        editor = "vim";
+        quotepath = false;
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      color = {
+        ui = true;
+      };
+      grep = {
+        lineNumber = true;
+      };
+      pull = {
+        ff = "only";
+      };
+      alias = {
+        fixup = "commit --all --amend";
+        empty = "commit --allow-empty -m 'Add an empty commit'";
+        current = "symbolic-ref --short HEAD";
+        "switch-default" = "!git checkout main 2>/dev/null || git checkout master 2>/dev/null";
+        upstream = "!git remote | grep -E '^upstream$'|| git remote | grep -E '^origin$'";
+        duster = "remote update origin --prune";
+        refresh = "!git switch-default && git pull \"$(git upstream)\" \"$(git current)\"";
+        all = "!git refresh && gh poi";
+        gui = "!lazygit";
+      };
+      gpg = {
+        format = "ssh";
+      };
+      "credential \"https-github.com\"" = {
+        helper = "!gh auth git-credential";
+      };
+      "credential \"https-gist.github.com\"" = {
+        helper = "!gh auth git-credential";
+      };
+    };
+  };
+
+  # https://github.com/nix-community/home-manager/blob/master/modules/programs/lazygit.nix
   programs.lazygit = {
     enable = true;
 
