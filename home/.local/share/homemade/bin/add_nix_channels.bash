d80ca8bb51697db85d28ea245b4efc7c0dcf3f89
Kenichi Kamiya <kachick1@gmail.com>
2023-08-10 03:58:52 +0900

    Bump home-manager

--- home/.local/share/homemade/bin/add_nix_channels.bash
+++ home/.local/share/homemade/bin/add_nix_channels.bash
@@ -8,7 +8,7 @@ set -euxo pipefail
 nix-channel --add https://releases.nixos.org/nixpkgs/nixpkgs-23.11pre509044.3acb5c4264c4/nixexprs.tar.xz nixpkgs
 # nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
 # nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
-nix-channel --add https://github.com/nix-community/home-manager/archive/729ab77f9e998e0989fa30140ecc91e738bc0cb1.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/a8f8f48320c64bd4e3a266a850bbfde2c6fe3a04.tar.gz home-manager
 nix-channel --update
 
 nix-channel --list
