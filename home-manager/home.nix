589c2387153e58f85646f24ca2585eb9c88b2be2
Kenichi Kamiya <kachick1@gmail.com>
2023-08-10 17:44:40 +0900

    Revert "Is this the answer for services?"
    This reverts commit fd259b39745161c08a5b533a9979b6a5ba246701.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,8 +16,6 @@
   # TODO: How to cover lima? The default is /home/kachick.local
   home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${config.home.username}" else "/home/${config.home.username}";
 
-  systemd.user.startServices = "sd-switch";
-
   # https://github.com/nix-community/home-manager/blob/master/modules/misc/xdg.nix
   xdg.enable = true;
 
