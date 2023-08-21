45cbf8d0057c4bfd7ac3a1242725d47353d39f88
Kenichi Kamiya <kachick1@gmail.com>
2023-08-21 12:09:21 +0900

    Enable emacs keybind also in PowerShell

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
+# Enable ctrl+a, ctrl+e
+Set-PSReadLineOption -EditMode Emacs
+
 # https://github.com/microsoft/winget-cli/issues/2498#issuecomment-1553863082
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
