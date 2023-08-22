3cb31abfb0f6b797d9726e36c815c8fc4afba0be
Kenichi Kamiya <kachick1@gmail.com>
2023-08-22 21:11:22 +0900

    Disable noisy beep(bell) in PowerShell
    #279
    https://github.com/kachick/times_kachick/issues/214

--- windows/config/Profile.ps1
+++ windows/config/Profile.ps1
@@ -1,5 +1,6 @@
 # Enable ctrl+a, ctrl+e
 Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineOption -BellStyle None
 
 # https://github.com/microsoft/winget-cli/issues/2498#issuecomment-1553863082
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
