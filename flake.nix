da581cd2acbea9d975e8f2bc57f7d2b4547a6f4b
Kenichi Kamiya <kachick1@gmail.com>
2023-08-23 13:29:27 +0900

    Bump go 1.21 (#286)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/0bab81e6319eb418e1509fe4b88401a022f3bf99' (2023-08-06)
      → 'github:NixOS/nixpkgs/0b07d4957ee1bd7fd3bdfd12db5f361bd70175a6' (2023-08-20)
    
    * Prefer nixos-unstable rather than nixpkgs-unstable
    
    Why nixos is edge for this time...?
    
    * Use go 1.21 in this repo
    
    * `go mod tidy -go=1.21`
    
    I dont know detail about toolchain...
    
    https://go.dev/doc/toolchain
    
    * Also bump winget importing golang
    
    * Also bump typos with the new nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
     # How to update the revision
     #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -29,7 +29,7 @@
               gitleaks
               cargo-make
               typos
-              go_1_20
+              go_1_21
 
               # To get sha256 around pkgs.fetchFromGitHub in CLI
               nix-prefetch-git
