0fc0c070dc430e1d064fa72f899dbc4b90e9af09
Kenichi Kamiya <kachick1@gmail.com>
2023-08-27 03:36:42 +0900

    Avoid to save several history in PowerShell

--- windows/config/Profile.ps1
+++ windows/config/Profile.ps1
@@ -1,6 +1,17 @@
 # Enable ctrl+a, ctrl+e
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadlineOption -BellStyle None
+Set-PSReadLineOption –HistoryNoDuplicates:$True
+
+Set-PSReadlineOption -AddToHistoryHandler {
+    param ($command)
+    switch -regex ($command) {
+        "winget install" {return $false}
+        "^[a-z]$" {return $false}
+        "exit" {return $false}
+    }
+    return $true
+}
 
 # https://github.com/microsoft/winget-cli/issues/2498#issuecomment-1553863082
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
