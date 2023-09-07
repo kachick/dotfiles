125bbb72ed85975936518880407d3008a9ed43f3
Kenichi Kamiya <kachick1@gmail.com>
2023-09-07 17:00:02 +0900

    Replace exa with eza

--- home-manager/homemade.nix
+++ home-manager/homemade.nix
@@ -53,7 +53,7 @@
 
       set -euo pipefail
 
-      ${lib.getExe pkgs.exa} --long --all --group-directories-first "$@"
+      ${lib.getExe pkgs.eza} --long --all --group-directories-first "$@"
     '';
   };
 
