e1612de86973fade460141435c8ed3c4a7b2240c
Kenichi Kamiya <kachick1@gmail.com>
2023-09-13 02:13:33 +0900

    Prefer simple URI

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -26,7 +26,7 @@
       user = {
         # - Visibility
         #   - https://stackoverflow.com/questions/48065535/should-i-keep-gitconfigs-signingkey-private
-        #   - ANYONE can access the registered public key at `curl -s https://api.github.com/users/kachick/gpg_keys | jq -r '.[0].raw_key'`
+        #   - ANYONE can access the registered public key at https://github.com/kachick.gpg
         # - Append `!` suffix for subkeys
         signingkey = "9BE4016A38165CCB!";
       };
