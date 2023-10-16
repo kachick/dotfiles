56d7fc08334254684c0655d3330a7796ee827e73
Kenichi Kamiya <kachick1@gmail.com>
2023-10-16 16:24:15 +0900

    Looks like needless escape in fish

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -52,7 +52,7 @@
 
     # `alias` will show current aliases
     shellAliases = {
-      gsc = "git commit -a -m \"\\`$(history --max 1)\\`\"";
+      gsc = "git commit -a -m \"`$(history --max 1)`\"";
     };
 
     plugins = [{
