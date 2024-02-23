6f9fdc99782c72b8716f9bee27ed10d426f265ce
Kenichi Kamiya <kachick1@gmail.com>
2024-02-23 20:39:11 +0900

    Prefer builtIn writeShellScript for shell scripting in nix files

--- home-manager/homemade.nix
+++ home-manager/homemade.nix
@@ -2,71 +2,51 @@
 
 # - Tiny tools by me, they may be rewritten with another language.
 {
-  xdg.dataFile."homemade/bin/bench_shells" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
+  xdg.dataFile."homemade/bin/bench_shells" = pkgs.writeShellScript "bench_shells.bash" ''
+    set -euo pipefail
 
-      set -euo pipefail
+    # ~ my feeling ~
+    # 50ms : blazing fast!
+    # 110ms : acceptable
+    # 150ms : slow
+    # 200ms : 1980s?
+    # 300ms : much slow!
 
-      # ~ my feeling ~
-      # 50ms : blazing fast!
-      # 110ms : acceptable
-      # 150ms : slow
-      # 200ms : 1980s?
-      # 300ms : much slow!
+    # zsh should be first, because it often makes much slower with the completion
+    ${lib.getBin pkgs.hyperfine}/bin/hyperfine --warmup 1 --runs 5 \
+      '${lib.getExe pkgs.zsh} --interactive -c exit' \
+      '${lib.getExe pkgs.bashInteractive} -i -c exit' \
+      '${lib.getExe pkgs.fish} --interactive --command exit'
+  '';
 
-      # zsh should be first, because it often makes much slower with the completion
-      ${lib.getBin pkgs.hyperfine}/bin/hyperfine --warmup 1 --runs 5 \
-        '${lib.getExe pkgs.zsh} --interactive -c exit' \
-        '${lib.getExe pkgs.bashInteractive} -i -c exit' \
-        '${lib.getExe pkgs.fish} --interactive --command exit'
-    '';
-  };
+  xdg.dataFile."homemade/bin/updeps" = pkgs.writeShellScript "updeps.bash" ''
+    set -euo pipefail
 
-  xdg.dataFile."homemade/bin/updeps" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
+    case ''${OSTYPE} in
+    linux*)
+      sudo apt update --yes && sudo apt upgrade --yes
+      ;;
+    darwin*)
+      softwareupdate --install --recommended
+      ;;
+    esac
 
-      set -euo pipefail
+    nix-channel --update
 
-      case ''${OSTYPE} in
-      linux*)
-      	sudo apt update --yes && sudo apt upgrade --yes
-      	;;
-      darwin*)
-      	softwareupdate --install --recommended
-      	;;
-      esac
+    ${lib.getExe pkgs.mise} plugins update
+  '';
 
-      nix-channel --update
+  xdg.dataFile."homemade/bin/la" = pkgs.writeShellScript "la.bash" ''
+    set -euo pipefail
 
-      ${lib.getExe pkgs.mise} plugins update
-    '';
-  };
+    ${lib.getBin pkgs.eza}/bin/eza --long --all --group-directories-first "$@"
+  '';
 
-  xdg.dataFile."homemade/bin/la" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
+  xdg.dataFile."homemade/bin/zj" = pkgs.writeShellScript "zj.bash" ''
+    set -euo pipefail
 
-      set -euo pipefail
+    name="$(${lib.getBin pkgs.coreutils}/bin/basename "$PWD")"
 
-      ${lib.getBin pkgs.eza}/bin/eza --long --all --group-directories-first "$@"
-    '';
-  };
-
-  xdg.dataFile."homemade/bin/zj" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
-
-      set -euo pipefail
-
-      name="$(${lib.getBin pkgs.coreutils}/bin/basename "$PWD")"
-
-      ${lib.getBin pkgs.zellij}/bin/zellij attach "$name" || ${lib.getBin pkgs.zellij}/bin/zellij --session "$name"
-    '';
-  };
+    ${lib.getBin pkgs.zellij}/bin/zellij attach "$name" || ${lib.getBin pkgs.zellij}/bin/zellij --session "$name"
+  '';
 }
