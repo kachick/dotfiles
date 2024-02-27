67a4c8314608948501a15aee67367209aa5ae345
Kenichi Kamiya <kachick1@gmail.com>
2024-02-27 13:01:37 +0900

    Alias podman with mise

--- home-manager/homemade.nix
+++ home-manager/homemade.nix
@@ -42,6 +42,15 @@
     ${lib.getBin pkgs.eza}/bin/eza --long --all --group-directories-first "$@"
   '';
 
+  # Why need the wrapper?
+  #   nixpkgs provide 4.9.3 is not including podman-remote.
+  #   https://github.com/NixOS/nixpkgs/blob/e3474e1d1e53b70e2b2af73ea26d6340e82f6b8b/pkgs/applications/virtualization/podman/default.nix#L104-L108
+  xdg.dataFile."homemade/bin/podman".source = pkgs.writeShellScript "podman.bash" ''
+    set -euo pipefail
+
+    ${lib.getBin pkgs.mise}/bin/mise exec podman@latest -- podman "$@"
+  '';
+
   xdg.dataFile."homemade/bin/zj".source = pkgs.writeShellScript "zj.bash" ''
     set -euo pipefail
 
