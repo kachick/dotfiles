a77483da50515dfdaf4175190522be04bae69c69
Kenichi Kamiya <kachick1@gmail.com>
2024-03-09 03:20:21 +0900

    clean up by root

---
+++ containers/cleanup.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# Focus to root privilege operations
+
+rm -rf /provisioner/dotfiles
+nix store gc
--- containers/needs_systemd.bash
+++ containers/needs_systemd.bash
@@ -4,6 +4,3 @@ set -euxo pipefail
 
 nix-shell --packages git --command 'git config --global --add safe.directory /provisioner/dotfiles'
 nix run '/provisioner/dotfiles#home-manager' -- switch -b backup --flake '/provisioner/dotfiles/#user'
-
-rm -rf /provisioner/dotfiles
-nix store gc
