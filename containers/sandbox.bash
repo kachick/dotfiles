d0b517d9128c429e02ccdc3951fbfb6286fca677
Kenichi Kamiya <kachick1@gmail.com>
2024-03-09 13:34:59 +0900

    Fix test script
    -it here enables colors atleast in local

--- containers/sandbox.bash
+++ containers/sandbox.bash
@@ -7,7 +7,7 @@ sandbox() {
 	sleep 1
 	local -r container_name="$(podman ps --sort=created --format '{{.Names}}' | tail -1)"
 	[ -n "$container_name" ]
-	podman exec --user=user --workdir='/home/user' -it "$container_name" /home/user/.nix-profile/bin/zsh
+	podman exec --user=user --workdir='/home/user' -it "$container_name" '/home/user/.nix-profile/bin/zsh'
 	podman kill "$container_name"
 }
 
