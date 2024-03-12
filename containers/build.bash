ca0295ad92e9218e08935420e3301422de79d9cc
Kenichi Kamiya <kachick1@gmail.com>
2024-03-12 12:37:15 +0900

    Fix cleanup of build scripts from container

--- containers/build.bash
+++ containers/build.bash
@@ -7,7 +7,7 @@ build() {
 	local -r container_id="$(podman run --detach --rm localhost/nix-systemd:latest)"
 	sleep 1 # Wait for the systemd to be ready
 	podman exec --user=user -it "$container_id" /provisioner/needs_systemd.bash
-	podman exec --user=root -it "$container_id" rm -rf /provisioner/cleanup.bash
+	podman exec --user=root -it "$container_id" rm -rf /provisioner
 	podman commit "$container_id" provisioned-systemd-home
 	podman kill "$container_id"
 }
