917109aa4c25d699ab3d97c1d98743932bbecddc
Kenichi Kamiya <kachick1@gmail.com>
2024-03-15 02:20:10 +0900

    Replace deprecated pinentryFlavor with pinentryPackage

--- home-manager/gpg.nix
+++ home-manager/gpg.nix
@@ -30,7 +30,9 @@
     defaultCacheTtl = 60480000; # 700 days
     maxCacheTtl = 60480000; # 700 days
 
-    pinentryFlavor = "tty";
+    # https://github.com/NixOS/nixpkgs/pull/294937
+    # https://github.com/nix-community/home-manager/pull/4895
+    pinentryPackage = pkgs.pinentry-tty;
   };
 
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/gpg.nix
