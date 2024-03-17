b4dd6d5f7ebcb3b9e0ca0ab133e6aadd4fe1d2b0
Kenichi Kamiya <kachick1@gmail.com>
2024-03-17 14:01:28 +0900

    As same as

--- containers/build.bash
+++ containers/build.bash
@@ -5,7 +5,8 @@ set -euxo pipefail
 build() {
 	podman build --tag nix-systemd --file containers/Containerfile .
 	local -r container_id="$(podman run --detach --rm localhost/nix-systemd:latest)"
-	trap 'podman kill "$container_id"' EXIT ERR
+	# shellcheck disable=SC2064
+	trap "podman kill '$container_id'" EXIT ERR
 	sleep 1 # Wait for the systemd to be ready
 	podman exec --user=user -it "$container_id" /provisioner/needs_systemd.bash
 	podman exec --user=root -it "$container_id" rm -rf /provisioner
--- containers/sandbox.bash
+++ containers/sandbox.bash
@@ -4,7 +4,8 @@ set -euo pipefail
 
 sandbox() {
 	local -r container_id="$(podman run --detach --rm localhost/provisioned-systemd-home)"
-	trap 'podman kill "$container_id"' EXIT ERR
+	# shellcheck disable=SC2064
+	trap "podman kill '$container_id'" EXIT ERR
 	sleep 1 # Wait for the systemd to be ready
 	podman exec --user=user --workdir='/home/user' -it "$container_id" '/home/user/.nix-profile/bin/zsh'
 }
