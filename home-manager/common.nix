485e1c032df2204d6e9e5d7af2149ff6df0e2e0e
Kenichi Kamiya <kachick1@gmail.com>
2024-03-18 17:14:41 +0900

    Install micro editor also in Windows
    Updates #446

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -165,6 +165,7 @@
     };
   };
 
+  # TODO: Consider to extract from nix managed, because of now also using in windows
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/micro.nix
   # https://github.com/zyedidia/micro/blob/c15abea64c20066fc0b4c328dfabd3e6ba3253a0/runtime/help/options.md
   programs.micro = {
