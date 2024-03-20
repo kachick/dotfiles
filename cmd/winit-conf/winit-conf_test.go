5147c1245523046514b5313d7e198dfb3f2c5982
Kenichi Kamiya <kachick1@gmail.com>
2024-03-20 23:18:45 +0900

    Simplify use of winit-conf with $PROFILE

--- cmd/winit-conf/winit-conf_test.go
+++ cmd/winit-conf/winit-conf_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestAssets(t *testing.T) {
-	for _, p := range provisioners("dummy_path") {
+	for _, p := range provisioners() {
 		_, err := config.WindowsAssets.ReadFile(p.EmbedPath())
 		if err != nil {
 			t.Fatalf("embed file not found: %v", err)
