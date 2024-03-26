8e914a972309fbb8f44761379fe78baa2577f6aa
Kenichi Kamiya <kachick1@gmail.com>
2024-03-27 02:45:55 +0900

    Add comment

--- home-manager/homemade.nix
+++ home-manager/homemade.nix
@@ -142,13 +142,16 @@ in
       name = "fzt-bind-posix-shell-history-to-git-commit-message";
       runtimeInputs = with pkgs; [ git fzf ruby_3_3 ];
       text = ''
+        # Avoiding nested single quote use
         bind="$(
         cat<<'EOF'
         enter:become(echo '`{}`' | git commit -a -F -)
         EOF
         )"
 
-        # bash keeps whitespace prefix even specified -n option for fc -l
+        # Why ruby?
+        # - bash keeps whitespace prefix even specified -n option for fc -l
+        # - lstrip is not enough for some history
         # shellcheck disable=SC2016 disable=SC2086
         ruby -pe '$_.strip!' | \
           fzf --height ''${FZF_TMUX_HEIGHT:-40%} ''${FZF_DEFAULT_OPTS-} -n2..,.. --scheme=history --bind "$bind"
