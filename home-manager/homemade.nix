97843288c228b7223ffcd97597f25f87d45486d3
Kenichi Kamiya <kachick1@gmail.com>
2024-03-26 18:08:47 +0900

    Suppress shellcheck here

--- home-manager/homemade.nix
+++ home-manager/homemade.nix
@@ -159,6 +159,7 @@ in
       text = ''
         cd "$(
           ghq list --full-path --exact "$(
+            # shellcheck disable=SC2016
             ghq list "$1" | fzf --delimiter / --nth 2.. --preview 'la "$(
               ghq list --full-path --exact {}
             )"' --preview-window '~3'
