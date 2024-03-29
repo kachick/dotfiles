ce5e514a9c4e0b331a460b9639712d16f08fff72
Kenichi Kamiya <kachick1@gmail.com>
2024-03-30 01:52:39 +0900

    WIP [ci skip]

--- flake.nix
+++ flake.nix
@@ -16,101 +16,105 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, flake-utils, my-nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        my-pkgs = my-nixpkgs.legacyPackages.${system};
-      in
-      rec {
-        # Q. Why nixpkgs-fmt? Not nixfmt? and alejandra?
-        # A. nixfmt will be official, but too opinionated and non stable now
-        # - https://github.com/NixOS/nixfmt/issues/153
-        # - https://github.com/NixOS/nixfmt/issues/129
-        # - https://github.com/NixOS/rfcs/pull/166
-        # - https://github.com/NixOS/nixfmt/blob/a81f922a2b362f347a6cbecff5fb14f3052bc25d/README.md#L19
-        formatter = pkgs.nixpkgs-fmt;
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs = [
-              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-              bashInteractive
-              nixpkgs-fmt
-              nixfmt # Using a sub formatter
-              nil
+  outputs = { self, nixpkgs, home-manager, my-nixpkgs }:
+    let
 
-              dprint
-              shellcheck
-              shfmt
-              gitleaks
-              cargo-make
-              typos
-              go_1_22
-              goreleaser
-              trivy
-
-              # To get sha256 around pkgs.fetchFromGitHub in CLI
-              nix-prefetch-git
-              jq
-            ];
-          };
-
-        packages.homeConfigurations = {
-          kachick = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [ ./home-manager/kachick.nix ];
-            extraSpecialArgs = { inherit my-pkgs; };
-          };
-
-          github-actions = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-              # Prefer "kachick" over "common" only here.
-              # Using values as much as possible as actual values to create a robust CI
-              ./home-manager/kachick.nix
-              { home.username = "runner"; }
-            ];
-            extraSpecialArgs = { inherit my-pkgs; };
-          };
-
-          user = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-              ./home-manager/common.nix
-              {
-                # "user" is default in podman-machine-default
-                home.username = "user";
-              }
-            ];
-            extraSpecialArgs = { inherit my-pkgs; };
-          };
-        };
-
-        packages.bump_completions =
-          pkgs.writeShellScriptBin "bump_completions" ''
-            set -euo pipefail
-
-            ${pkgs.podman}/bin/podman completion bash > ./dependencies/podman/completions.bash
-            ${pkgs.podman}/bin/podman completion zsh > ./dependencies/podman/completions.zsh
-            ${pkgs.podman}/bin/podman completion fish > ./dependencies/podman/completions.fish
-
-            ${pkgs.dprint}/bin/dprint completions bash > ./dependencies/dprint/completions.bash
-            ${pkgs.dprint}/bin/dprint completions zsh > ./dependencies/dprint/completions.zsh
-            ${pkgs.dprint}/bin/dprint completions fish > ./dependencies/dprint/completions.fish
-          '';
-
-        apps = {
-          # example: `nix run .#home-manager -- switch -n -b backup --flake .#kachick`
-          # https://github.com/NixOS/nix/issues/6448#issuecomment-1132855605
-          home-manager = flake-utils.lib.mkApp {
-            drv = home-manager.defaultPackage.${system};
-          };
-
-          bump_completions = {
-            type = "app";
-            program = "${packages.bump_completions}/bin/bump_completions";
-          };
-        };
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+        my-nixpkgs = import my-nixpkgs { inherit system; };
       });
+    in
+    packages = forAllSystems ({ pkgs }: {
+  rec {
+  # Q. Why nixpkgs-fmt? Not nixfmt? and alejandra?
+  # A. nixfmt will be official, but too opinionated and non stable now
+  # - https://github.com/NixOS/nixfmt/issues/153
+  # - https://github.com/NixOS/nixfmt/issues/129
+  # - https://github.com/NixOS/rfcs/pull/166
+  # - https://github.com/NixOS/nixfmt/blob/a81f922a2b362f347a6cbecff5fb14f3052bc25d/README.md#L19
+  formatter = pkgs.nixpkgs-fmt;
+  devShells.default = with pkgs;
+    mkShell {
+      buildInputs = [
+        # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+        bashInteractive
+        nixpkgs-fmt
+        nixfmt # Using a sub formatter
+        nil
+
+        dprint
+        shellcheck
+        shfmt
+        gitleaks
+        cargo-make
+        typos
+        go_1_22
+        goreleaser
+        trivy
+
+        # To get sha256 around pkgs.fetchFromGitHub in CLI
+        nix-prefetch-git
+        jq
+      ];
+    };
+
+  packages.homeConfigurations = {
+    kachick = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./home-manager/kachick.nix ];
+      extraSpecialArgs = { inherit my-pkgs; };
+    };
+
+    github-actions = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        # Prefer "kachick" over "common" only here.
+        # Using values as much as possible as actual values to create a robust CI
+        ./home-manager/kachick.nix
+        { home.username = "runner"; }
+      ];
+      extraSpecialArgs = { inherit my-pkgs; };
+    };
+
+    user = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        ./home-manager/common.nix
+        {
+          # "user" is default in podman-machine-default
+          home.username = "user";
+        }
+      ];
+      extraSpecialArgs = { inherit my-pkgs; };
+    };
+  };
+
+  packages.bump_completions =
+    pkgs.writeShellScriptBin "bump_completions" ''
+      set -euo pipefail
+
+      ${pkgs.podman}/bin/podman completion bash > ./dependencies/podman/completions.bash
+      ${pkgs.podman}/bin/podman completion zsh > ./dependencies/podman/completions.zsh
+      ${pkgs.podman}/bin/podman completion fish > ./dependencies/podman/completions.fish
+
+      ${pkgs.dprint}/bin/dprint completions bash > ./dependencies/dprint/completions.bash
+      ${pkgs.dprint}/bin/dprint completions zsh > ./dependencies/dprint/completions.zsh
+      ${pkgs.dprint}/bin/dprint completions fish > ./dependencies/dprint/completions.fish
+    '';
+
+  apps = {
+    # example: `nix run .#home-manager -- switch -n -b backup --flake .#kachick`
+    # https://github.com/NixOS/nix/issues/6448#issuecomment-1132855605
+    home-manager = flake-utils.lib.mkApp {
+      drv = home-manager.defaultPackage.${system};
+    };
+
+    bump_completions = {
+      type = "app";
+      program = "${packages.bump_completions}/bin/bump_completions";
+    };
+  };
+}
 }
 
