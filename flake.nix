3390db02594e6cccb6e6f069d9ef64f8f848618e
Kenichi Kamiya <kachick1@gmail.com>
2024-03-31 21:18:54 +0900

    Revert "How about use nixos-unstable in home-manager?"
    This reverts commit de2d84d311857289bbed39706fa2f42cfeb6ec4b.

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     home-manager = {
       # candidates: "github:nix-community/home-manager/release-23.05";
       url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "edge-nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
