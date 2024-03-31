e12216cbff2e7b4b1ad26dcbe25f648891abbb31
Kenichi Kamiya <kachick1@gmail.com>
2024-03-31 19:40:26 +0900

    Assume nix store path but check any xz are

--- flake.nix
+++ flake.nix
@@ -104,12 +104,9 @@
             name = "check_no_dirty_xz_in_nix_store";
             runtimeInputs = with pkgs; [ fd ];
             text = ''
-              if [ -n "$NIX_STORE" ]; then
-                nix_store_path="$NIX_STORE"
-              else
-                nix_store_path=/nix/store
-              fi
-              fd '^\w+-xz-5\.6\.[01]\.drv' --search-path "$nix_store_path" --has-results && exit 1
+              # nix store should have xz: https://github.com/NixOS/nixpkgs/blob/b96bc828b81140dd3fb096b4e66a6446d6d5c9dc/doc/stdenv/stdenv.chapter.md?plain=1#L177
+              fd '^\w+-xz-[0-9\.]+\.drv' --search-path /nix/store --has-results || exit 1
+              fd '^\w+-xz-5\.6\.[01]\.drv' --search-path /nix/store --has-results && exit 1
             '';
             meta = {
               description = "Prevent #530 (around CVE-2024-3094)";
