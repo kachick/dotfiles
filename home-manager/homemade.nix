d934d8969ed4c7a13a87ccfff1f32e693c21e92f
Kenichi Kamiya <kachick1@gmail.com>
2024-04-01 04:01:18 +0900

    Upload home-manager generated configs as an artifact

--- home-manager/homemade.nix
+++ home-manager/homemade.nix
@@ -207,4 +207,21 @@ in
       };
     }
   )
+
+  (pkgs.writeShellApplication
+    {
+      name = "archive-home-files";
+      runtimeInputs = with pkgs; [ gnutar ripgrep coreutils ];
+      text = ''
+        archive_basename="$1"
+        # * Basically used in container for backup use, so keep fixed file name for the archive
+        # * Specify the subdir of hm-gen/home-files, the dereference and recursive from generation root doesn't end
+        tar --create --file="''${archive_basename}.tar.gz" --auto-compress --dereference --recursive --verify \
+          --directory="$(home-manager generations | rg 'id \d+ -> /nix/store' | head -n1 | cut -d ' ' -f 7)/home-files" .
+      '';
+      meta = {
+        description = "Backup dotfiles they are generated with home-manager. See #243";
+      };
+    }
+  )
 ]
