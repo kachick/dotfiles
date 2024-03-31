02facfd9f92084b054ea4eab3818ab4d4f05f8a0
Kenichi Kamiya <kachick1@gmail.com>
2024-03-31 22:50:13 +0900

    Log detail for the checker

--- flake.nix
+++ flake.nix
@@ -116,7 +116,7 @@
             text = ''
               # nix store should have xz: https://github.com/NixOS/nixpkgs/blob/b96bc828b81140dd3fb096b4e66a6446d6d5c9dc/doc/stdenv/stdenv.chapter.md?plain=1#L177
               fd '^\w+-xz-[0-9\.]+\.drv' --search-path /nix/store --has-results || exit 1
-              fd '^\w+-xz-5\.6\.[01]\.drv' --search-path /nix/store --has-results && exit 1
+              fd '^\w+-xz-5\.6\.[01]\.drv' --search-path /nix/store --max-results=1 && exit 1
             '';
             meta = {
               description = "Prevent #530 (around CVE-2024-3094)";
