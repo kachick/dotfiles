873e06c1644661f82cd17865246258703cd50ea6
Kenichi Kamiya <kachick1@gmail.com>
2024-04-04 00:22:46 +0900

    Remove duplicated shell option

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -43,8 +43,6 @@
         runtimeInputs = with pkgs; [ git edge-pkgs.typos ];
         # What arguments: https://git-scm.com/docs/githooks#_post_checkout
         text = ''
-          set -o pipefail
-
           new_head="$2"
           is_file_checkout="$3" # 0: file, 1: branch
           if [[ "$is_file_checkout" -eq 0 ]]; then
