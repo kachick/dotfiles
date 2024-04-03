3cc4c7076e329215230600b4331e86b77e04f6ba
Kenichi Kamiya <kachick1@gmail.com>
2024-04-04 00:40:23 +0900

    Fix a typo
    Reported to typos

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -29,7 +29,7 @@
 
     # - They will be overridden by local hooks, currently I do nothing to merge global and local hooks
     #   If I want it, https://github.com/timokau/dotfiles/blob/dfee6670a42896cfb5a94fdedf96c9ed2fa1c9d2/home/git.nix#L3-L11 may be a good example
-    # - I don't have confident for executable permissions are reqiored or not for them, removing it worked. :<
+    # - I don't have confident for executable permissions are required or not for them, removing it worked. :<
     hooks = {
       commit-msg = pkgs.writeShellScript "prevent_typos_in_commit_mssage.bash" ''
         set -euo pipefail
