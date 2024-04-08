a892a461e1ce53df35cd97c878ce1d5547af2496
Kenichi Kamiya <kachick1@gmail.com>
2024-04-08 15:59:56 +0900

    Do not trim line-end for piping

--- home-manager/homemade.nix
+++ home-manager/homemade.nix
@@ -162,8 +162,9 @@ in
       # Why ruby?
       # - bash keeps whitespace prefix even specified -n option for fc -l
       # - lstrip is not enough for some history
+      # - Keep line-end in fzf input
       # shellcheck disable=SC2016 disable=SC2086
-      ruby -pe '$_.strip!' | \
+      ruby -e 'STDIN.each { |line| puts line.strip }' | \
         fzf --height ''${FZF_TMUX_HEIGHT:-40%} ''${FZF_DEFAULT_OPTS-} -n2..,.. --scheme=history --bind "$bind"
     '';
     meta = {
