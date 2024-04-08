2615e9204e5cd5653806f9b5f82cf5401a91dfd6
Kenichi Kamiya <kachick1@gmail.com>
2024-04-09 02:13:14 +0900

    Update and prune in `git refresh(all)`

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -59,8 +59,7 @@ in
       current = "symbolic-ref --short HEAD";
       switch-default = "!git switch main 2>/dev/null || git switch master 2>/dev/null";
       upstream = "!git remote | grep -E '^upstream$'|| git remote | grep -E '^origin$'";
-      duster = "remote update origin --prune";
-      refresh = "!git switch-default && git pull --prune \"$(git upstream)\" \"$(git current)\"";
+      refresh = "!git remote update origin --prune && git switch-default && git pull --prune \"$(git upstream)\" \"$(git current)\"";
       all = "!git refresh && git-delete-merged-branches";
       # Do not add `--graph`, it makes too slow in large repository as NixOS/nixpkgs
       pp = "log --format='format:%C(cyan)%ad %C(auto)%h %C(auto)%s %C(auto)%d' --date=short --color=always";
