899df920a8885e7938a81fdb3b6771319710e9c2
Kenichi Kamiya <kachick1@gmail.com>
2024-04-09 17:17:25 +0900

    Provide preview window in todo command

--- home-manager/homemade.nix
+++ home-manager/homemade.nix
@@ -187,10 +187,14 @@ in
       git
       edge-pkgs.fzf
       micro
+      bat
     ];
+    # open bat with line number in preview: https://github.com/sharkdp/bat/issues/1185#issuecomment-1301473901
     text = ''
       git grep --perl-regexp --line-number --column --color=always '\b(?<=TODO|FIXME|BUG)\b\S+' | \
-        fzf --ansi --delimiter : --nth 4.. --bind 'enter:become(micro -parsecursor=true {1}:{2}:{3})'
+        fzf --ansi --delimiter : --nth 4.. \
+          --preview 'bat {1} --color=always --highlight-line={2}' --preview-window='~3,+{2}+3/4' \
+          --bind 'enter:become(micro -parsecursor=true {1}:{2}:{3})'
     '';
     meta = {
       description = "List todo family";
