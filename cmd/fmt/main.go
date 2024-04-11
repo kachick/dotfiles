b527747817f71fc216d93909ee2967cc53e9d0c6
Kenichi Kamiya <kachick1@gmail.com>
2024-04-11 14:20:06 +0900

    Tuning to make faster fmt

--- cmd/fmt/main.go
+++ cmd/fmt/main.go
@@ -3,11 +3,17 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/kachick/dotfiles/internal/fileutils"
 	"github.com/kachick/dotfiles/internal/runner"
 )
 
 func main() {
+	allFlag := flag.Bool("all", false, "includes heavy linters")
+
+	flag.Parse()
+
 	walker := fileutils.GetWalker()
 
 	bashPaths := walker.GetAllBash()
@@ -17,9 +23,13 @@ func main() {
 	cmds := runner.Commands{
 		{Path: "dprint", Args: []string{"fmt"}},
 		{Path: "shfmt", Args: append([]string{"--language-dialect", "bash", "--write"}, bashPaths...)},
-		// nix fmt doesn't respect .gitignore, without paths, .direnv included: https://github.com/NixOS/nixfmt/issues/151
-		{Path: "nix", Args: append([]string{"fmt"}, nixPaths...)},
 		{Path: "go", Args: []string{"fmt", "./..."}},
+	}
+
+	if *allFlag {
+		// nix fmt doesn't respect .gitignore, without paths, .direnv included: https://github.com/NixOS/nixfmt/issues/151
+		// nixfmt-rfc-style looks like using cache, if update with empty commit, it takes longtime
+		cmds = append(cmds, runner.Cmd{Path: "nix", Args: append([]string{"fmt"}, nixPaths...)})
 	}
 
 	cmds.ParallelRun()
