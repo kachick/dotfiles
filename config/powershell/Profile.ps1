07ecaf166d9cf62d440ce5af0fcb95b28bc910d6
Kenichi Kamiya <kachick1@gmail.com>
2024-04-15 21:31:10 +0900

    Always inject winget path in PowerShell

--- config/powershell/Profile.ps1
+++ config/powershell/Profile.ps1
@@ -18,12 +18,9 @@ function la {
     Get-ChildItem -Force
 }
 
-if (Get-Command -ErrorAction SilentlyContinue starship) {
-    Invoke-Expression (&starship init powershell)
-} else {
-    # https://github.com/microsoft/winget-cli/issues/2498#issuecomment-1553863082
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-}
+# https://github.com/microsoft/winget-cli/issues/2498#issuecomment-1553863082
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+Invoke-Expression (&starship init powershell)
 
 # https://github.com/kachick/PSFzfHistory
 Set-FzfHistoryKeybind -Chord Ctrl+r
