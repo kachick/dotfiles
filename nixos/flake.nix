674f73b2f8505fd9c687362a90d675bc8cc59e6b
Kenichi Kamiya <kachick1@gmail.com>
2024-04-28 23:01:08 +0900

    Translate xreamp YAML config with Nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,6 +5,8 @@
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     # unstable-pkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # https://github.com/xremap/nix-flake/blob/master/docs/HOWTO.md
+    xremap-flake.url = "github:xremap/nix-flake";
   };
 
   outputs =
@@ -12,6 +14,7 @@
       self,
       nixpkgs,
       # unstable-pkgs,
+      xremap-flake,
       ...
     }@inputs:
     {
@@ -24,6 +27,36 @@
           # Import the previous configuration.nix we used,
           # so the old configuration file still takes effect
           ./configuration.nix
+          xremap-flake.nixosModules.default
+          {
+            # Modmap for single key rebinds
+            services.xremap.config = {
+              modmap = [
+                {
+                  name = "Global";
+                  remap = {
+                    "CapsLock" = "Ctrl_L";
+                    "Alt_L" = {
+                      "held" = "Alt_L";
+                      "alone" = "Muhenkan";
+                      "alone_timeout_millis" = 500;
+                    };
+                    "Alt_R" = "Henkan";
+                  };
+                }
+              ];
+
+              # Keymap for key combo rebinds
+              keymap = [
+                {
+                  name = "Gnome lancher";
+                  remap = {
+                    "Alt-Space" = "LEFTMETA";
+                  };
+                }
+              ];
+            };
+          }
         ];
       };
     };
