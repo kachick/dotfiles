82127e2c8f54dc1ee291a820d3ec2c02cd401a75
Kenichi Kamiya <kachick1@gmail.com>
2024-05-16 17:08:47 +0900

    Inline systems in flake schema and drop flake-utils (#525)
    Drop flake-utils dependency to specify systems in flake schema
    
    Referenced resources
    
    * https://github.com/Misterio77/nix-starter-configs/tree/f1ecf7e2275f541af7bec763866a909224b937a4/standard
    * https://github.com/viperML/neohome/blob/d8d6ee78690f1230f3d91051b2220357b01dd6e4/content/blog/flake-utils/index.md?plain=1#L2

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
     #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     edge-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     # https://github.com/nix-community/home-manager/blob/release-23.11/docs/manual/nix-flakes.md
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
@@ -21,138 +20,149 @@
       nixpkgs,
       edge-nixpkgs,
       home-manager,
-      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        edge-pkgs = edge-nixpkgs.legacyPackages.${system};
-      in
-      rec {
-        # nixfmt will be official
-        # - https://github.com/NixOS/nixfmt/issues/153
-        # - https://github.com/NixOS/nixfmt/issues/129
-        # - https://github.com/NixOS/rfcs/pull/166
-        # - https://github.com/NixOS/nixfmt/blob/a81f922a2b362f347a6cbecff5fb14f3052bc25d/README.md#L19
-        formatter = edge-pkgs.nixfmt-rfc-style;
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-              bashInteractive
-              edge-pkgs.nixfmt-rfc-style
-              edge-pkgs.nil
-              # To get sha256 around pkgs.fetchFromGitHub in CLI
-              nix-prefetch-git
-              jq
+    let
+      # Candidates: https://github.com/NixOS/nixpkgs/blob/release-23.11/lib/systems/flake-systems.nix
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-darwin"
+        # I don't have M1+ mac, providing this for macos-14 free runner https://github.com/actions/runner-images/issues/9741
+        "aarch64-darwin"
+      ];
+    in
+    rec {
+      # nixfmt will be official
+      # - https://github.com/NixOS/nixfmt/issues/153
+      # - https://github.com/NixOS/nixfmt/issues/129
+      # - https://github.com/NixOS/rfcs/pull/166
+      # - https://github.com/NixOS/nixfmt/blob/a81f922a2b362f347a6cbecff5fb14f3052bc25d/README.md#L19
+      formatter = forAllSystems (system: edge-nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
 
-              shellcheck
-              shfmt
-              gitleaks
-              cargo-make
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          edge-pkgs = edge-nixpkgs.legacyPackages.${system};
+        in
+        {
+          default =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+                bashInteractive
+                edge-pkgs.nixfmt-rfc-style
+                edge-pkgs.nil
+                # To get sha256 around pkgs.fetchFromGitHub in CLI
+                nix-prefetch-git
+                jq
 
-              edge-pkgs.dprint
-              edge-pkgs.typos
-              edge-pkgs.go_1_22
-              edge-pkgs.goreleaser
-              edge-pkgs.trivy
-            ];
-          };
+                shellcheck
+                shfmt
+                gitleaks
+                cargo-make
 
-        packages.homeConfigurations = {
-          kachick = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [ ./home-manager/kachick.nix ];
-            extraSpecialArgs = {
-              inherit edge-pkgs;
+                edge-pkgs.dprint
+                edge-pkgs.typos
+                edge-pkgs.go_1_22
+                edge-pkgs.goreleaser
+                edge-pkgs.trivy
+              ];
             };
-          };
+        }
+      );
 
-          wsl = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-              ./home-manager/kachick.nix
-              ./home-manager/wsl.nix
-            ];
-            extraSpecialArgs = {
-              inherit edge-pkgs;
-            };
-          };
+      packages = forAllSystems (
+        system:
+        (import ./pkgs {
+          pkgs = nixpkgs.legacyPackages.${system};
+          edge-pkgs = edge-nixpkgs.legacyPackages.${system};
+        })
+      );
 
-          github-actions = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-              # Prefer "kachick" over "common" only here.
-              # Using values as much as possible as actual values to create a robust CI
-              ./home-manager/kachick.nix
-              { home.username = "runner"; }
-            ];
-            extraSpecialArgs = {
-              inherit edge-pkgs;
-            };
-          };
+      apps = forAllSystems (system: {
+        # example: `nix run .#home-manager -- switch -n -b backup --flake .#user@linux`
+        # https://github.com/NixOS/nix/issues/6448#issuecomment-1132855605
+        home-manager = {
+          type = "app";
+          program = "${home-manager.defaultPackage.${system}}/bin/home-manager";
+        };
 
-          user = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-              ./home-manager/common.nix
-              { home.username = "user"; }
-            ];
-            extraSpecialArgs = {
-              inherit edge-pkgs;
-            };
+        bump_completions = {
+          type = "app";
+          program = "${packages.${system}.bump_completions}/bin/bump_completions";
+        };
+
+        check_no_dirty_xz_in_nix_store = {
+          type = "app";
+          program = "${packages.${system}.check_no_dirty_xz_in_nix_store}/bin/check_no_dirty_xz_in_nix_store";
+        };
+      });
+
+      homeConfigurations = {
+        "kachick@linux" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [ ./home-manager/kachick.nix ];
+          extraSpecialArgs = {
+            edge-pkgs = edge-nixpkgs.legacyPackages.x86_64-linux;
           };
         };
 
-        packages.bump_completions = pkgs.writeShellScriptBin "bump_completions" ''
-          set -euo pipefail
-
-          ${edge-pkgs.podman}/bin/podman completion bash > ./dependencies/podman/completions.bash
-          ${edge-pkgs.podman}/bin/podman completion zsh > ./dependencies/podman/completions.zsh
-          ${edge-pkgs.podman}/bin/podman completion fish > ./dependencies/podman/completions.fish
-
-          ${edge-pkgs.dprint}/bin/dprint completions bash > ./dependencies/dprint/completions.bash
-          ${edge-pkgs.dprint}/bin/dprint completions zsh > ./dependencies/dprint/completions.zsh
-          ${edge-pkgs.dprint}/bin/dprint completions fish > ./dependencies/dprint/completions.fish
-        '';
-
-        packages.check_no_dirty_xz_in_nix_store = pkgs.writeShellApplication {
-          name = "check_no_dirty_xz_in_nix_store";
-          runtimeInputs = with pkgs; [ fd ];
-          text = ''
-            # nix store should have xz: https://github.com/NixOS/nixpkgs/blob/b96bc828b81140dd3fb096b4e66a6446d6d5c9dc/doc/stdenv/stdenv.chapter.md?plain=1#L177
-            # You can't use --max-results instead of --has-results even if you want the log, it always returns true
-            fd '^\w+-xz-[0-9\.]+\.drv' --search-path /nix/store --has-results
-
-            # Why toggling errexit and return code here: https://github.com/kachick/times_kachick/issues/278
-            set +o errexit
-            fd '^\w+-xz-5\.6\.[01]\.drv' --search-path /nix/store --has-results
-            fd_return_code="$?" # Do not directly use the $? to prevent feature broken if inserting another command before check
-            set -o errexit
-            [[ "$fd_return_code" -eq 1 ]]
-          '';
-          meta = {
-            description = "Prevent #530 (around CVE-2024-3094)";
+        "kachick@wsl" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            ./home-manager/kachick.nix
+            ./home-manager/wsl.nix
+          ];
+          extraSpecialArgs = {
+            edge-pkgs = edge-nixpkgs.legacyPackages.x86_64-linux;
           };
         };
 
-        apps = {
-          # example: `nix run .#home-manager -- switch -n -b backup --flake .#kachick`
-          # https://github.com/NixOS/nix/issues/6448#issuecomment-1132855605
-          home-manager = flake-utils.lib.mkApp { drv = home-manager.defaultPackage.${system}; };
-
-          bump_completions = {
-            type = "app";
-            program = "${packages.bump_completions}/bin/bump_completions";
-          };
-
-          check_no_dirty_xz_in_nix_store = {
-            type = "app";
-            program = "${packages.check_no_dirty_xz_in_nix_store}/bin/check_no_dirty_xz_in_nix_store";
+        "kachick@macbook" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+          modules = [ ./home-manager/kachick.nix ];
+          extraSpecialArgs = {
+            edge-pkgs = edge-nixpkgs.legacyPackages.x86_64-darwin;
           };
         };
-      }
-    );
+
+        "github-actions@ubuntu-24.04" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            # Prefer "kachick" over "common" only here.
+            # Using values as much as possible as actual values to create a robust CI
+            ./home-manager/kachick.nix
+            { home.username = "runner"; }
+          ];
+          extraSpecialArgs = {
+            edge-pkgs = edge-nixpkgs.legacyPackages.x86_64-linux;
+          };
+        };
+
+        "github-actions@macos-14" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+          modules = [
+            # Prefer "kachick" over "common" only here.
+            # Using values as much as possible as actual values to create a robust CI
+            ./home-manager/kachick.nix
+            { home.username = "runner"; }
+          ];
+          extraSpecialArgs = {
+            edge-pkgs = edge-nixpkgs.legacyPackages.aarch64-darwin;
+          };
+        };
+
+        "user@linux" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            ./home-manager/common.nix
+            { home.username = "user"; }
+          ];
+          extraSpecialArgs = {
+            edge-pkgs = edge-nixpkgs.legacyPackages.x86_64-linux;
+          };
+        };
+      };
+    };
 }
