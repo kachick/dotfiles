5216778a99fd2106df3ed3446b51cf5e6850ea3b
Kenichi Kamiya <kachick1@gmail.com>
2024-06-18 15:44:34 +0900

    Add comment

--- pkgs/trim-github-user-prefix-for-reponame/main.go
+++ pkgs/trim-github-user-prefix-for-reponame/main.go
@@ -11,6 +11,7 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
+		// Using Cut cannot handle `multiple sep as foo/bar/baz`, but I don't need to conisder it for `owner/repo` or `repo` patterns.
 		_, after, found := strings.Cut(line, "/")
 		if found {
 			fmt.Println(after)
