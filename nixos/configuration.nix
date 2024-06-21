426b70205d8703a0a0e0a2b2764fc7b2aa8f9417
Kenichi Kamiya <kachick1@gmail.com>
2024-06-21 22:21:31 +0900

    Follow new schema for xkb.*

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -56,8 +56,10 @@
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
   # Enable CUPS to print documents.
