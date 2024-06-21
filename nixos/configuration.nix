195a89eec83400548f26bb99d29fa4ae3b49db78
Kenichi Kamiya <kachick1@gmail.com>
2024-06-21 23:04:28 +0900

    How about installing os layer?

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -143,6 +143,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     micro
+    wezterm
     alacritty
     ibm-plex
 
