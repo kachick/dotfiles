919ae093616b8b7fafd2b60c7d02d0fa8e51720a
Kenichi Kamiya <kachick1@gmail.com>
2024-06-24 04:55:30 +0900

    Update new schema for KDE on NixOS24.05

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -55,8 +55,8 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
