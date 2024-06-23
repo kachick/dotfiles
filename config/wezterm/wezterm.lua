2b35c97dc7a5243838f2cdd8f59a15ab87de4959
Kenichi Kamiya <kachick1@gmail.com>
2024-06-24 04:40:31 +0900

    Disable wayland for wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -37,7 +37,8 @@ end
 -- Using in wayland requires non released versions
 -- https://github.com/wez/wezterm/issues/5340
 if string.find(wezterm.target_triple, "-linux", 1, true) then
-  config.enable_wayland = true
+  -- wezterem often broken on wayland, especially always on hyprland https://github.com/wez/wezterm/issues/4857#issuecomment-1911269113
+  config.enable_wayland = false
 end
 
 config.keys = {
