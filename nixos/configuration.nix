cb0ceb31fd0e5156c07baf10f7579bc1a1fb56b8
Kenichi Kamiya <kachick1@gmail.com>
2024-06-24 04:05:06 +0900

    Update new schema for libinput

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,7 +109,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users = {
