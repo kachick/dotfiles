75a8a1f24e7f4587fc064f04bbc83aa3723d8ba2
Kenichi Kamiya <kachick1@gmail.com>
2024-06-24 16:04:33 +0900

    gdm is needed...

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -57,8 +57,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.displayManager.sddm.wayland.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
   services = {
     desktopManager.plasma6.enable = true;
