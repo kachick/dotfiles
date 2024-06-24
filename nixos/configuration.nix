6f4d457b4893f0f951c8820b491aea3ee1bdfe3a
Kenichi Kamiya <kachick1@gmail.com>
2024-06-24 16:34:01 +0900

    I hope this affects natural scrolling in KDE

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,7 +140,10 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput = {
+    enable = true;
+    mouse.naturalScrolling = true;
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users = {
