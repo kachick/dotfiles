d94b55b1854705e50908edd9dd8feeb959b4f50d
Kenichi Kamiya <kachick1@gmail.com>
2024-06-24 16:00:31 +0900

    How about enabling as this?

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -57,15 +57,15 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
   services = {
     desktopManager.plasma6.enable = true;
-    displayManager = {
-      sddm.enable = false;
-      # gdm.enable = true;
-      defaultSession = "plasma";
-    };
+    # displayManager = {
+    #   sddm.enable = true;
+    #   defaultSession = "plasma";
+    # };
   };
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
