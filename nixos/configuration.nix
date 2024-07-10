de5c4eb5c7a9b1a1436579b816be4e66c75f6975
Kenichi Kamiya <kachick1@gmail.com>
2024-07-10 22:10:25 +0900

    Disable unstanle kmscon
    looks not active in this 10 years
    
    https://cgit.freedesktop.org/~dvdhrm/kmscon/

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -383,15 +383,15 @@ in
     keyMap = "us";
   };
 
-  # Better console appearance even if no X
+  # Better console appearance even if no X, but do not use for now with the unstable behaviors
   # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/ttys/kmscon.nix
   services.kmscon = {
-    enable = true;
-    hwRender = true;
+    enable = false;
+    hwRender = false;
     fonts = with pkgs; [
       {
-        name = "PlemolJP Console NF";
-        package = plemoljp-nf;
+        name = "IBM Plex Mono";
+        package = ibm-plex;
       }
       {
         name = "Noto Color Emoji";
