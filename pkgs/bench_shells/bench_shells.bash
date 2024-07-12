f98b8e5fd66f175a60dac085d6105989778fb01e
Kenichi Kamiya <kachick1@gmail.com>
2024-07-12 19:39:35 +0900

    Extract bash comments to markdown

--- pkgs/bench_shells/bench_shells.bash
+++ pkgs/bench_shells/bench_shells.bash
@@ -1,10 +1,3 @@
-# ~ my feeling ~
-# 50ms : blazing fast!
-# 110ms : acceptable
-# 150ms : slow
-# 200ms : 1980s?
-# 300ms : much slow!
-
 # zsh should be first, because it often makes much slower with the completion
 hyperfine --warmup 1 --runs 5 \
 	'zsh --interactive -c exit' \
