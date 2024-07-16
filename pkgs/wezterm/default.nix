2dddf78959162b4495e885bc8861ca96857ae244
Kenichi Kamiya <kachick1@gmail.com>
2024-07-16 21:07:41 +0900

    Prefer simple shell wrapping again

--- pkgs/wezterm/default.nix
+++ pkgs/wezterm/default.nix
@@ -1,16 +1,14 @@
 { inputs, pkgs, ... }:
 
-# Using latest to avoid stable release and wayland problems https://github.com/wez/wezterm/issues/5340
-inputs.wezterm-flake.packages.${pkgs.system}.default.overrideAttrs (prev: {
-  nativeBuildInputs = prev.nativeBuildInputs ++ [ pkgs.makeWrapper ];
-
+pkgs.writeShellApplication {
+  name = "wezterm";
   # Wrap for https://github.com/wez/wezterm/pull/4777#issuecomment-2014478175
-  postFixup = ''
-    makeShellWrapper $out/bin/wezterm $out/bin/wezterm-wrapped \
-        --set WAYLAND_DISPLAY 1
+  text = ''
+    export WAYLAND_DISPLAY=1
+    wezterm "$@"
   '';
-
-  meta = prev.meta // {
-    mainProgram = "wezterm-wrapped";
-  };
-})
+  runtimeInputs = [
+    # Using latest to avoid stable release and wayland problems https://github.com/wez/wezterm/issues/5340
+    inputs.wezterm-flake.packages.${pkgs.system}.default
+  ];
+}
