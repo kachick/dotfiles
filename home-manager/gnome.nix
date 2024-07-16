fb97f9a3e8347ea4506b2f10ebb97fd3b26098f2
Kenichi Kamiya <kachick1@gmail.com>
2024-07-17 04:40:34 +0900

    Clarify enabling dconf by home-manager

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -2,15 +2,18 @@
 
 {
   # https://github.com/nix-community/home-manager/blob/release-24.05/modules/misc/dconf.nix
-  dconf.settings = {
-    "org/gnome/shell" = {
-      enabled-extensions = [
-        "blur-my-shell@aunetx"
-        "pop-shell@system76.com"
-      ];
-    };
-    "org/gnome/shell/extensions/pop-shell" = {
-      tile-by-default = true;
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/shell" = {
+        enabled-extensions = [
+          "blur-my-shell@aunetx"
+          "pop-shell@system76.com"
+        ];
+      };
+      "org/gnome/shell/extensions/pop-shell" = {
+        tile-by-default = true;
+      };
     };
   };
 }
