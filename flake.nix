0e72445fc6b95dbcc0aefae184d0e5bf0d69d679
Kenichi Kamiya <kachick1@gmail.com>
2024-07-18 03:56:22 +0900

    Inject nixd for the LSP that only one of zed nix extension supporting
    https://github.com/zed-extensions/nix/blob/242631811daee74d5ee26be36a067e4033e88bd3/extension.toml#L13-L15

--- flake.nix
+++ flake.nix
@@ -73,7 +73,8 @@
                 # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
                 bashInteractive
                 nixfmt-rfc-style
-                nil
+                nil # TODO: Used in vscode Nix extension. Consider to replace with nixd: https://github.com/oxalica/nil/issues/111
+                nixd # Used in zed Nix extension
                 nixpkgs-lint-community
                 # To get sha256 around pkgs.fetchFromGitHub in CLI
                 nix-prefetch-git
