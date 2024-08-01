5eb0a44ef2930f1ec7380b23cebc4489a48db547
Kenichi Kamiya <kachick1@gmail.com>
2024-08-01 14:12:34 +0900

    Fix quote of command written in ENV

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -65,8 +65,10 @@
     # And it does not have the feature to specify environments. So needed to set here
     # https://github.com/germanztz/gnome-shell-extension-rclone-manager/blob/72f1a2ac4a1205069bc2bda5d1e5906e83a2b4ab/fileMonitorHelper.js#L124-L1>
     # https://github.com/germanztz/gnome-shell-extension-rclone-manager/blob/72f1a2ac4a1205069bc2bda5d1e5906e83a2b4ab/fileMonitorHelper.js#L594
+    #
+    # For implementation, --name should be quoted with double quote, quoting with single does not work...
     RCLONE_PASSWORD_COMMAND = ''
-      ${lib.getExe edge-pkgs.goldwarden} logins get --name 'Rclone - Configuration Password'
+      ${lib.getExe edge-pkgs.goldwarden} logins get --name "Rclone - Configuration Password"
     '';
   };
 
