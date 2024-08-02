f7481a220ffe91c113969f861eac8a5e3efb8e0e
Kenichi Kamiya <kachick1@gmail.com>
2024-08-02 16:24:41 +0900

    Missing enable gtk

--- home-manager/ime.nix
+++ home-manager/ime.nix
@@ -2,6 +2,7 @@
 {
   # https://github.com/nix-community/home-manager/blob/release-24.05/modules/misc/gtk.nix
   gtk = {
+    enable = true;
     # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland
     gtk2.extraConfig = ''
       gtk-im-module=fcitx
