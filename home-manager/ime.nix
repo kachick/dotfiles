c55fdd4b3e3480d8a1a3f72aaa15d713b8d7dc49
Kenichi Kamiya <kachick1@gmail.com>
2024-08-02 16:22:18 +0900

    gkt module only exsits in home-manager

---
+++ home-manager/ime.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  # https://github.com/nix-community/home-manager/blob/release-24.05/modules/misc/gtk.nix
+  gtk = {
+    # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland
+    gtk2.extraConfig = ''
+      gtk-im-module=fcitx
+    '';
+    gtk3.extraConfig = {
+      gtk-im-module = "fcitx";
+    };
+    gtk4.extraConfig = {
+      gtk-im-module = "fcitx";
+    };
+  };
+}
