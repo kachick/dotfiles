bf5843a2d482c286008546229e2eb01980c9b119
Kenichi Kamiya <kachick1@gmail.com>
2024-08-04 22:30:35 +0900

    Assign Super+Num keybindings to switch/mode workspaces

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -51,8 +51,57 @@
         # exec-arg="";
       };
 
+      # gsettings list-recursively | grep -F "<Super>"
+      # Disabling defaults to enable Suprt+num family will be used to switch workspaces
+      # https://github.com/pop-os/shell/issues/142
+      "org/gnome/shell/keybindings" = {
+        switch-to-application-1 = [ ];
+        switch-to-application-2 = [ ];
+        switch-to-application-3 = [ ];
+        switch-to-application-4 = [ ];
+        switch-to-application-5 = [ ];
+        switch-to-application-6 = [ ];
+        switch-to-application-7 = [ ];
+        switch-to-application-8 = [ ];
+        switch-to-application-9 = [ ];
+
+        open-new-window-application-1 = [ ];
+        open-new-window-application-2 = [ ];
+        open-new-window-application-3 = [ ];
+        open-new-window-application-4 = [ ];
+        open-new-window-application-5 = [ ];
+        open-new-window-application-6 = [ ];
+        open-new-window-application-7 = [ ];
+        open-new-window-application-8 = [ ];
+        open-new-window-application-9 = [ ];
+      };
+
+      "org/gnome/desktop/wm/preferences" = {
+        num-workspaces = 9;
+      };
+
       "org/gnome/desktop/wm/keybindings" = {
         toggle-message-tray = [ "<Shift><Super>m" ]; # default: ['<Super>v', '<Super>m'], `"disable"` restore default. So added annoy modifier to prevent trigger
+
+        switch-to-workspace-1 = "['<Super>1']";
+        switch-to-workspace-2 = "['<Super>2']";
+        switch-to-workspace-3 = "['<Super>3']";
+        switch-to-workspace-4 = "['<Super>4']";
+        switch-to-workspace-5 = "['<Super>5']";
+        switch-to-workspace-6 = "['<Super>6']";
+        switch-to-workspace-7 = "['<Super>7']";
+        switch-to-workspace-8 = "['<Super>8']";
+        switch-to-workspace-9 = "['<Super>9']";
+
+        move-to-workspace-1 = "['<Super><Shift>1']";
+        move-to-workspace-2 = "['<Super><Shift>2']";
+        move-to-workspace-3 = "['<Super><Shift>3']";
+        move-to-workspace-4 = "['<Super><Shift>4']";
+        move-to-workspace-5 = "['<Super><Shift>5']";
+        move-to-workspace-6 = "['<Super><Shift>6']";
+        move-to-workspace-7 = "['<Super><Shift>7']";
+        move-to-workspace-8 = "['<Super><Shift>8']";
+        move-to-workspace-9 = "['<Super><Shift>9']";
       };
 
       "org/gnome/settings-daemon/plugins/media-keys" = {
@@ -95,6 +144,8 @@
 
       "org/gnome/mutter" = {
         experimental-features = [ "scale-monitor-framebuffer" ];
+
+        dynamic-workspaces = false;
       };
 
       "org/gnome/desktop/interface" = {
