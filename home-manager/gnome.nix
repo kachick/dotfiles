5a2995b98c34cdc9390d775dfcacc70665f87d96
Kenichi Kamiya <kachick1@gmail.com>
2024-08-05 01:33:39 +0900

    Fix pop-shell and GNOME basics conflicting (#719)
    * Install pop-launcher
    
    * Fix conflicting pop-shell and gnome keybindigns
    
    * Assign Super+Num keybindings to switch/mode workspaces
    
    * Fix syntax of specifying keybinds
    
    * I cannot manage many workspaces
    
    * Disable Alt-Space by xremap
    
    * Disable default Alt+space gnome keybinds
    
    * Disable removed custom keybind
    
    * Enable keybinding to launch Firefox again
    
    * Do not disable non conflicting keybinds
    
    * Restore dropped custom keybinding for alacritty
    
    * Use Nordic gnome theme to avoid pop-shell launcher styling problem
    
    * Fix broken style combination with pop-shell launcher and gnome
    
    Switching theme is not enough
    
    * Install color pickers
    
    * Disable conflicting color picker extension by default
    
    * Adjust search color with Nordic

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -34,6 +34,7 @@
             auto-move-windows
             just-perfection
             dash-to-dock
+            # color-picker # Don't enable by default. It conflicts with clipboard-history
           ]
         );
 
@@ -45,14 +46,72 @@
         ];
       };
 
+      # https://unix.stackexchange.com/questions/481142/launch-default-terminal-emulator-by-command
+      "org/gnome/desktop/default-applications/terminal" = {
+        exec = lib.getExe pkgs.alacritty;
+        # exec-arg="";
+      };
+
+      # gsettings list-recursively | grep -F "<Super>"
+      # Disabling defaults to enable Suprt+num family will be used to switch workspaces
+      # https://github.com/pop-os/shell/issues/142
+      "org/gnome/shell/keybindings" = {
+        switch-to-application-1 = [ ];
+        switch-to-application-2 = [ ];
+        switch-to-application-3 = [ ];
+        # switch-to-application-4 = [ ];
+        # switch-to-application-5 = [ ];
+        # switch-to-application-6 = [ ];
+        # switch-to-application-7 = [ ];
+        # switch-to-application-8 = [ ];
+        # switch-to-application-9 = [ ];
+
+        open-new-window-application-1 = [ ];
+        open-new-window-application-2 = [ ];
+        open-new-window-application-3 = [ ];
+        # open-new-window-application-4 = [ ];
+        # open-new-window-application-5 = [ ];
+        # open-new-window-application-6 = [ ];
+        # open-new-window-application-7 = [ ];
+        # open-new-window-application-8 = [ ];
+        # open-new-window-application-9 = [ ];
+      };
+
+      "org/gnome/desktop/wm/preferences" = {
+        num-workspaces = 3;
+      };
+
       "org/gnome/desktop/wm/keybindings" = {
+        activate-window-menu = [ ]; # Disabling default `<Alt>space` to run launchers
+
         toggle-message-tray = [ "<Shift><Super>m" ]; # default: ['<Super>v', '<Super>m'], `"disable"` restore default. So added annoy modifier to prevent trigger
+
+        switch-to-workspace-1 = [ "<Super>1" ];
+        switch-to-workspace-2 = [ "<Super>2" ];
+        switch-to-workspace-3 = [ "<Super>3" ];
+        # switch-to-workspace-4 = [ "<Super>4" ];
+        # switch-to-workspace-5 = [ "<Super>5" ];
+        # switch-to-workspace-6 = [ "<Super>6" ];
+        # switch-to-workspace-7 = [ "<Super>7" ];
+        # switch-to-workspace-8 = [ "<Super>8" ];
+        # switch-to-workspace-9 = [ "<Super>9" ];
+
+        move-to-workspace-1 = [ "<Super><Shift>1" ];
+        move-to-workspace-2 = [ "<Super><Shift>2" ];
+        move-to-workspace-3 = [ "<Super><Shift>3" ];
+        # move-to-workspace-4 = [ "<Super><Shift>4" ];
+        # move-to-workspace-5 = [ "<Super><Shift>5" ];
+        # move-to-workspace-6 = [ "<Super><Shift>6" ];
+        # move-to-workspace-7 = [ "<Super><Shift>7" ];
+        # move-to-workspace-8 = [ "<Super><Shift>8" ];
+        # move-to-workspace-9 = [ "<Super><Shift>9" ];
       };
 
       "org/gnome/settings-daemon/plugins/media-keys" = {
-        control-center = [ "<Super>comma" ];
-        www = [ "<Super>w" ];
-        search = [ "<Super>f" ];
+        # control-center = [ "<Super>comma" ]; # I set this because of inspired by vscode, but disable to avoid conflict of pop-shell minimizerr
+        www = [ "<Super>w" ]; # Prefer w even through pop-shell recommends to Super+b
+        # terminal = [ "<Super>t" ]; I don't know why this won't work. So use cosutom keybinding
+        # search = [ "<Super>f" ]; # pop-shell sets to file manager
         custom-keybindings = [
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
@@ -60,19 +119,25 @@
       };
 
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        name = "Terminal";
+        name = "Alacritty";
         binding = "<Super>t";
         command = lib.getExe pkgs.alacritty;
       };
 
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
         name = "Resource Monitor - TUI";
-        binding = "<Super>b"; # I don't know why, <Super>m don't work even if removed other shortcuts :<
+        binding = "<Super>r";
         command = "${lib.getExe pkgs.alacritty} --command=${lib.getExe pkgs.bottom} --title='Resource Monitor(btm)'";
       };
 
+      # https://github.com/pop-os/shell/blob/master_noble/schemas/org.gnome.shell.extensions.pop-shell.gschema.xml
       "org/gnome/shell/extensions/pop-shell" = {
         tile-by-default = true;
+
+        # Keybindings: https://github.com/pop-os/shell/blob/master_noble/scripts/configure.sh
+
+        # https://www.reddit.com/r/pop_os/comments/mt5kgf/how_to_change_default_keybind_for/
+        activate-launcher = [ "<Alt>space" ];
       };
 
       "org/gnome/shell/extensions/clipboard-history" = {
@@ -89,6 +154,8 @@
 
       "org/gnome/mutter" = {
         experimental-features = [ "scale-monitor-framebuffer" ];
+
+        dynamic-workspaces = false;
       };
 
       "org/gnome/desktop/interface" = {
@@ -97,6 +164,8 @@
 
         # https://unix.stackexchange.com/questions/327975/how-to-change-the-gnome-panel-time-format
         clock-show-weekday = true;
+
+        gtk-theme = "Nordic";
       };
 
       "org/gnome/shell/extensions/dash-to-dock" = {
@@ -108,6 +177,9 @@
         multi-monitor = true;
       };
 
+      "org/gnome/desktop/wm/preferences" = {
+        theme = "Nordic";
+      };
     };
   };
 }
