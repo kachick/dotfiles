d2d439245b553f79d435f15387e7eaaaf4e1b669
Kenichi Kamiya <kachick1@gmail.com>
2024-08-07 22:01:25 +0900

    shrug

--- pkgs/walk/main.go
+++ pkgs/walk/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/mattn/go-pipeline"
 )
@@ -15,9 +16,16 @@ func main() {
 	flag.Parse()
 
 	workingDirectory := *wdirFlag
+	var queries []string
 
-	query := os.Args[1]
-	// queries := os.Args[1:]
+	if len(os.Args) > 2 {
+		queries = os.Args[2:]
+	} else {
+		queries = []string{}
+	}
+
+	// query := os.Args[2]
+
 	// queries := []string{"l", "m"}
 
 	// strings.Join(queries, " ")
@@ -29,7 +37,7 @@ func main() {
 			"fd", "--type", "f", "--hidden", "--follow", "--exclude", ".git", ".", workingDirectory,
 		},
 		[]string{
-			"fzf", "--query", query, "--preview", "bat --color=always {}", "--preview-window", "~3", "--bind", "enter:become(command \"$EDITOR\" {})",
+			"fzf", "--query", strings.Join(queries, " "), "--preview", "bat --color=always {}", "--preview-window", "~3", "--bind", "enter:become(command \"$EDITOR\" {})",
 		},
 	)
 
