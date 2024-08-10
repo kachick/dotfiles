2eee401fd7b346561096a525e77f112bd6e2875d
Kenichi Kamiya <kachick1@gmail.com>
2024-08-11 03:42:12 +0900

    I may retire on this way

--- pkgs/walk/walk.bash
+++ pkgs/walk/walk.bash
@@ -1,19 +1,19 @@
 parser_definition() {
-	setup REST help:usage -- "Usage: walk [options]... [fuzzy queries]..." ''
-	msg -- 'Options:'
-	# Don't use $PWD to avoid printing needless absolute path for current directory
-	option working_directory -d --working_directory on:"." -- "working directory"
-	disp :usage --help
+  setup REST help:usage -- "Usage: walk [options]... [arguments]..." ''
+  msg -- 'Options:'
+  # Don't use $PWD to avoid printing needless absolute path for current directory
+  option WALK_FROM -d --working-directory on:"/tmp" -- "Working Directory"
+  disp :usage --help
 }
 
 eval "$(getoptions parser_definition) exit 1"
 
-if [ $# -ge 1 ]; then
-	query="$1"
-else
-	query=""
-fi
+# if [ $# -ge 1 ]; then
+#   query="$1"
+# else
+#   query=""
+# fi
 
-# shellcheck disable=SC2016,SC2154
-fd --type f --hidden --follow --exclude .git . "$working_directory" |
-	fzf --query "$query" --preview 'bat --color=always {}' --preview-window '~3' --bind 'enter:become(command "$EDITOR" {})'
+# shellcheck disable=SC2016
+fd --type f --hidden --follow --exclude .git . "$WALK_FROM" |
+  fzf --query "$@" --preview 'bat --color=always {}' --preview-window '~3' --bind 'enter:become(command "$EDITOR" {})'
