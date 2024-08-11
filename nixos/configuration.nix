da2c80b5b2431aec2bfc8fcb948e3391a80c77fc
Kenichi Kamiya <kachick1@gmail.com>
2024-08-11 15:56:26 +0900

    Enable Firefox NixOS module to install language packs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -282,6 +282,15 @@
 
   programs.nix-ld.enable = false;
 
+  # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/programs/firefox.nix
+  programs.firefox = {
+    enable = true;
+    languagePacks = [
+      "en-US"
+      "ja"
+    ];
+  };
+
   # Prefer NixOS modules rather than home-manager for easy setting up
   programs.goldwarden = {
     package = edge-pkgs.goldwarden;
