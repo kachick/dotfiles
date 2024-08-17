9749072035c58f79e0c016eac096718d12d020ad
Kenichi Kamiya <kachick1@gmail.com>
2024-08-17 22:09:21 +0900

    Extract fzf config with the referenced ratio

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -16,6 +16,7 @@
     ./ssh.nix
     ./git.nix
     ./editors.nix
+    ./fzf.nix
     ./firefox.nix
     ./linux.nix
     ./darwin.nix
@@ -172,36 +173,6 @@
   };
   xdg.configFile."fcitx5/profile" = {
     source = ../config/fcitx5/profile;
-  };
-
-  # https://github.com/nix-community/home-manager/blob/release-24.05/modules/programs/fzf.nix
-  # https://github.com/junegunn/fzf/blob/master/README.md
-  programs.fzf = rec {
-    enable = true;
-
-    # https://github.com/junegunn/fzf/blob/d579e335b5aa30e98a2ec046cb782bbb02bc28ad/README.md#respecting-gitignore
-    defaultCommand = "${pkgs.fd}/bin/fd --type f --strip-cwd-prefix --hidden --follow --exclude .git";
-
-    defaultOptions = [
-      # --walker*: Default file filtering will be changed by this option if FZF_DEFAULT_COMMAND is not set: https://github.com/junegunn/fzf/pull/3649/files
-      "--walker-skip '.git,node_modules,.direnv,vendor,dist'"
-    ];
-
-    # CTRL+T
-    fileWidgetCommand = defaultCommand;
-    fileWidgetOptions = [
-      "--preview '${pkgs.bat}/bin/bat --color=always {}'"
-      "--preview-window '~3'"
-    ];
-
-    # ALT-C
-    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d --hidden --exclude .git";
-    changeDirWidgetOptions = [ "--preview '${pkgs.eza}/bin/eza --color=always --tree {} | head -200'" ];
-
-    colors = {
-      # See #295 for the detail
-      "bg+" = "#005f5f";
-    };
   };
 
   # https://github.com/nix-community/home-manager/blob/release-24.05/modules/programs/starship.nix
