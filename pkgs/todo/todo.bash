73d723b9ce808c96c7aa9be284fd696c6b74d580
Kenichi Kamiya <kachick1@gmail.com>
2024-08-24 16:38:15 +0900

    Make todo command possible to take query from CLI
    Want this when addressing GH-757

--- pkgs/todo/todo.bash
+++ pkgs/todo/todo.bash
@@ -1,6 +1,6 @@
 # open bat with line number in preview: https://github.com/sharkdp/bat/issues/1185#issuecomment-1301473901
 # shellcheck disable=SC2016
 git grep --perl-regexp --line-number --column --color=always '\b(?<=TODO|FIXME|BUG)\b\S+' |
-	fzf --ansi --delimiter : --nth 4.. \
+	fzf --ansi --delimiter : --nth 4.. --query "$@" \
 		--preview 'bat {1} --color=always --highlight-line={2}' --preview-window='~3,+{2}+3/4' \
 		--bind 'enter:become(command "$EDITOR" {1}:{2}:{3})'
