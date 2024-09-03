1c49aff7b9295cb5961e076fb84a7b5a44a6578d
Kenichi Kamiya <kachick1@gmail.com>
2024-09-03 16:21:55 +0900

    Fix to register zellij bash comletions

--- pkgs/posix_shared_functions/register_completions.bash
+++ pkgs/posix_shared_functions/register_completions.bash
@@ -1,3 +1,3 @@
 complete -F _fzf_complete_makers -o default -o bashdefault makers
 complete -F _fzf_complete_task -o default -o bashdefault task
-complete -F _fzf_complete_zellij -o default -o bashdefault task
+complete -F _fzf_complete_zellij -o default -o bashdefault zellij
