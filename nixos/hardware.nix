a4ddf09acca1596984f683efdb44dbf9882de0fe
Kenichi Kamiya <kachick1@gmail.com>
2024-09-16 02:30:14 +0900

    Don't depend on broken services.udev.extraHwdb

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -1,14 +1,23 @@
 { ... }:
 {
-  services.udev = {
-    enable = true;
-    # Settings keyremap in raw layer than X. See GH-784
-    #
-    # Specify hardware names even if `evdev:input:*` working for mostcase
-    extraHwdb = ''
-      evdev:name:Topre REALFORCE 87 US:*
-        # original: capslock
-        KEYBOARD_KEY_70039=leftctrl
-    '';
-  };
+  # services.udev = {
+  #   enable = true;
+  #   # Settings keyremap in raw layer than X. See GH-784
+
+  #   # Specify hardware names even if `evdev:input:*` working for mostcase
+  #   extraHwdb = ''
+  #     evdev:name:Topre REALFORCE 87 US:*
+  #       # original: capslock
+  #       KEYBOARD_KEY_70039=leftctrl
+  #   '';
+  # };
+
+  # Settings keyremap in raw layer than X. See GH-784
+  # Don't use `services.udev.extraHwdb`, it does not create the file at least in NixOS 24.05
+  # See https://github.com/NixOS/nixpkgs/issues/182966 for detail
+  environment.etc."udev/hwdb.d/99-local.hwdb".text = ''
+    evdev:name:Topre REALFORCE 87 US:*
+      # original: capslock
+      KEYBOARD_KEY_70039=leftctrl
+  '';
 }
