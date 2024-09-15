747068a95119b22b9fb227e4ffe3bf8c8aa62b6b
Kenichi Kamiya <kachick1@gmail.com>
2024-09-15 22:16:55 +0900

    Import AUR to build cozette psfu

--- pkgs/cozette/default.nix
+++ pkgs/cozette/default.nix
@@ -2,6 +2,9 @@
   lib,
   stdenvNoCC,
   fetchzip,
+  gawk,
+  gnused,
+  bdf2psf,
 }:
 
 # Patched because of nixpkgs does not include psf
@@ -17,6 +20,25 @@ stdenvNoCC.mkDerivation rec {
     hash = "sha256-Cnl7DTPcZmCRM06qe7WXfZorok3uUNYcB9bR/auzCao=";
   };
 
+  nativeBuildInputs = [
+    gawk
+    gnused
+    bdf2psf
+  ];
+
+  # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=psf-cozette
+  buildPhase = ''
+    # Confine Powerline left divider symbols to strictly 6 pixels wide
+    awk -i inplace 'BEGIN { l=-128 } $1=="ENCODING"&&($2==57520||$2==57521||$2==57524) { l=FNR } l+4<FNR&&FNR<=l+17 { printf("%02X\n", and(lshift(strtonum("0x"$1), 1), 0xFF)); next; }{ print }' cozette.bdf
+    awk -i inplace 'BEGIN { l=-128 } $1=="ENCODING"&&($2==57520||$2==57521||$2==57524) { l=FNR } l+4<FNR&&FNR<=l+30 { printf("%04X\n", and(lshift(strtonum("0x"$1), 1), 0xFFFF)); next; }{ print }' cozette_hidpi.bdf
+
+    # Fix for bdf2psf limitation (See https://github.com/slavfox/Cozette/issues/122#issuecomment-2165328416)
+    sed -i -e 's/^BBX [2-8]/BBX 9/g' cozette_hidpi.bdf
+
+    bdf2psf --fb cozette.bdf /usr/share/bdf2psf/standard.equivalents codepoints.set 512 cozette6x13.psfu
+    bdf2psf --fb cozette_hidpi.bdf /usr/share/bdf2psf/standard.equivalents codepoints.set 512 cozette12x26.psfu
+  '';
+
   installPhase = ''
     runHook preInstall
 
@@ -26,7 +48,7 @@ stdenvNoCC.mkDerivation rec {
     install -Dm644 *.otb -t $out/share/fonts/misc
     install -Dm644 *.woff -t $out/share/fonts/woff
     install -Dm644 *.woff2 -t $out/share/fonts/woff2
-    install -Dm644 *.psf -t $out/share/consolefonts
+    install -Dm644 *.psfu -t $out/share/consolefonts
 
     runHook postInstall
   '';
