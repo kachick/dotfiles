b06f725d56c788db5e1b8b06e8e74105fe4bedf6
Kenichi Kamiya <kachick1@gmail.com>
2024-09-16 17:33:56 +0900

    Temporary add hello only for home-manager package

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -111,6 +111,8 @@ with pkgs;
   #  - argos-translate - can be closed in offline, but not yet enough accuracy
   #  - Apertium - does not support Japanese
   translate-shell # `echo "$text" | trans en:ja`
+
+  hello # Test home-manager is active
 ]
 ++ (with homemade-pkgs; [
   la
