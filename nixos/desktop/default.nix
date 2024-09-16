504c284db6000fd31f009595f81dde6c7ce278b6
Kenichi Kamiya <kachick1@gmail.com>
2024-09-16 20:55:44 +0900

    Introduce newsflash for the RSS reader client in NixOS (#766)

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -145,6 +145,8 @@ in
 
       evtest # To debug keyremapper as GH-786
 
+      newsflash # RSS reader # TODO: Manage config (sqlite?) or Backup the exported OPML
+
       # Don't use unstable channel. It frequently backported to stable channel
       #   - https://github.com/NixOS/nixpkgs/commits/nixos-24.05/pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
       (signal-desktop.overrideAttrs (prev: {
