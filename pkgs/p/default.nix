a57d8b23bcda9225bf6447dea89c5ef2c02a67a6
Kenichi Kamiya <kachick1@gmail.com>
2024-09-26 16:53:31 +0900

    Clarify to run zsh
    Because of $SHELL is now bash
    Since GH-452

--- pkgs/p/default.nix
+++ pkgs/p/default.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 pkgs.writeShellApplication {
   name = "p";
-  runtimeInputs = with pkgs; [ nix ];
+  runtimeInputs = with pkgs; [
+    nix
+    zsh
+  ];
   text = ''
     # Needless to trim the default command, nix-shell only runs last command if given multiple.
-    nix-shell --command "$SHELL" --packages "$@"
+    nix-shell --command 'zsh' --packages "$@"
   '';
 }
