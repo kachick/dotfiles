691fc68473e554dd115ae233c99de328e0754e61
Kenichi Kamiya <kachick1@gmail.com>
2024-10-08 12:50:36 +0900

    Add tests for go pkgs

--- pkgs/trim-github-user-prefix-for-reponame/main.go
+++ pkgs/trim-github-user-prefix-for-reponame/main.go
@@ -7,16 +7,20 @@ import (
 	"strings"
 )
 
+func extractRepo(line string) string {
+	// Using Cut cannot handle `multiple sep as foo/bar/baz`, but I don't need to conisder it for `owner/repo` or `repo` patterns.
+	_, after, found := strings.Cut(line, "/")
+	if found {
+		return after
+	} else {
+		return line
+	}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
-		// Using Cut cannot handle `multiple sep as foo/bar/baz`, but I don't need to conisder it for `owner/repo` or `repo` patterns.
-		_, after, found := strings.Cut(line, "/")
-		if found {
-			fmt.Println(after)
-		} else {
-			fmt.Println(line)
-		}
+		fmt.Println(extractRepo(line))
 	}
 }
