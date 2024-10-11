0fe5efe5c3a5a9819474c54b2f1f638fe4587ddb
Kenichi Kamiya <kachick1@gmail.com>
2024-10-11 11:06:04 +0900

    Fix to specify uninstalling firefox package from home-manager

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -6,7 +6,10 @@ lib.mkMerge [
     # https://github.com/nix-community/home-manager/blob/release-24.05/modules/programs/firefox.nix
     programs.firefox = {
       enable = true;
-      package = pkgs.emptyDirectory;
+      # In firefox package, use null instead of `pkgs.emptyDirectory`
+      #   - https://github.com/kachick/dotfiles/pull/835#discussion_r1796307643
+      #   - https://github.com/nix-community/home-manager/blob/342a1d682386d3a1d74f9555cb327f2f311dda6e/modules/programs/firefox/mkFirefoxModule.nix#L264
+      package = null;
       profiles.default = {
         isDefault = true;
         settings = {
