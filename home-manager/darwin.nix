cdf510babac02df26cd644e314ee2522036a57b4
Kenichi Kamiya <kachick1@gmail.com>
2024-10-16 16:27:53 +0900

    Add Karabiner-Elements config to persist the keyremapper for disabling caps_lock on macOS (#848)

--- home-manager/darwin.nix
+++ home-manager/darwin.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 # https://github.com/nix-community/home-manager/issues/414#issuecomment-427163925
 lib.mkMerge [
@@ -20,13 +25,34 @@ lib.mkMerge [
       ];
     };
 
-    # https://github.com/NixOS/nixpkgs/issues/240819#issuecomment-1616760598
-    # https://github.com/midchildan/dotfiles/blob/fae87a3ef327c23031d8081333678f9472e4c0ed/nix/home/modules/gnupg/default.nix#L38
-    xdg.dataFile."gnupg/gpg-agent.conf".text = ''
-      grab
-      default-cache-ttl 604800
-      max-cache-ttl 604800
-      pinentry-program ${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
-    '';
+    xdg = {
+      configFile = {
+        "karabiner/assets" = {
+          source = ../config/karabiner/assets;
+          recursive = true;
+        };
+
+        "karabiner/HomeManagerInit_karabiner.json" = {
+          source = ../config/karabiner/karabiner.json;
+          # https://github.com/nix-community/home-manager/issues/3090#issuecomment-2010891733
+          onChange = ''
+            rm -f ${config.xdg.configHome}/karabiner/karabiner.json
+            cp ${config.xdg.configHome}/karabiner/HomeManagerInit_karabiner.json ${config.xdg.configHome}/karabiner/karabiner.json
+            chmod u+w ${config.xdg.configHome}/karabiner/karabiner.json
+          '';
+        };
+      };
+
+      dataFile = {
+        # https://github.com/NixOS/nixpkgs/issues/240819#issuecomment-1616760598
+        # https://github.com/midchildan/dotfiles/blob/fae87a3ef327c23031d8081333678f9472e4c0ed/nix/home/modules/gnupg/default.nix#L38
+        "gnupg/gpg-agent.conf".text = ''
+          grab
+          default-cache-ttl 604800
+          max-cache-ttl 604800
+          pinentry-program ${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
+        '';
+      };
+    };
   })
 ]
