9ae28bae78094b5f5457491898384180364e16d4
Kenichi Kamiya <kachick1@gmail.com>
2024-10-22 18:36:47 +0900

    Fix with avoiding missing xdg.stateHome

--- home-manager/vim.nix
+++ home-manager/vim.nix
@@ -6,7 +6,8 @@
 }:
 
 {
-  xdg.stateFile."vim/.keep".text = "Keep this directory because of home-manager and vim does not create the file if directory is missing";
+  # TODO: Prefer xdg.stateFile since home-manager release-24.11. See https://github.com/nix-community/home-manager/pull/5779
+  home.file."${config.xdg.stateHome}/vim/.keep".text = "Keep this directory because of home-manager and vim does not create the file if directory is missing";
 
   # https://github.com/nix-community/home-manager/blob/release-24.05/modules/programs/vim.nix
   # https://nixos.wiki/wiki/Vim
