cf75c40913ac58384da95befee01b7c385e20a32
Kenichi Kamiya <kachick1@gmail.com>
2024-10-22 18:38:40 +0900

    Make .viminfo respect XDG Base Directory (#877)
    * Make .viminfo respect XDG Base Directory
    
    Fixes GH-871
    
    * Ensure to put vim directory in statehome
    
    * Fix with avoiding missing xdg.stateHome

--- home-manager/vim.nix
+++ home-manager/vim.nix
@@ -1,6 +1,14 @@
-{ pkgs, homemade-pkgs, ... }:
+{
+  pkgs,
+  config,
+  homemade-pkgs,
+  ...
+}:
 
 {
+  # TODO: Prefer xdg.stateFile since home-manager release-24.11. See https://github.com/nix-community/home-manager/pull/5779
+  home.file."${config.xdg.stateHome}/vim/.keep".text = "Keep this directory because of home-manager and vim does not create the file if directory is missing";
+
   # https://github.com/nix-community/home-manager/blob/release-24.05/modules/programs/vim.nix
   # https://nixos.wiki/wiki/Vim
   programs.vim = {
@@ -20,6 +28,7 @@
     extraConfig = ''
       colorscheme iceberg
       set termguicolors
+      set viminfofile=${config.xdg.stateHome}/vim/viminfo
     '';
   };
 }
