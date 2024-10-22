5516ac38e42115debb847791b1c6a543c5a2bb8d
Kenichi Kamiya <kachick1@gmail.com>
2024-10-22 17:09:57 +0900

    Remove needless dependencies since using ruby for glue engine

--- pkgs/envs/default.nix
+++ pkgs/envs/default.nix
@@ -3,9 +3,7 @@ pkgs.writeShellApplication rec {
   name = "envs";
   text = builtins.readFile ./${name}.bash;
   runtimeInputs = with pkgs; [
-    coreutils # `sort`, `tr`
     fzf
-    findutils # `xargs
     ruby_3_3 # pkgs.writers.writeRuby and writeRubyBin does not fit
   ];
   runtimeEnv = {
