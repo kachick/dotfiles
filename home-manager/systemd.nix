b89d0a3a6dc1310c32454ec91d2e3e2e6fcb1143
Kenichi Kamiya <kachick1@gmail.com>
2024-10-23 21:31:12 +0900

    Set GNOME specific LC_TIME in environment.d

--- home-manager/systemd.nix
+++ home-manager/systemd.nix
@@ -4,6 +4,14 @@
   # https://github.com/nix-community/home-manager/blob/release-24.05/modules/systemd.nix#L161-L173
   systemd = {
     user = {
+      # Enable locale for GNOME, terminals respect home.sessionVariables instead
+      # https://superuser.com/a/1841368
+      # https://github.com/nix-community/home-manager/blob/5ec753a1fc4454df9285d8b3ec0809234defb975/modules/systemd.nix#L92-L96
+      sessionVariables = {
+        # GNOME clock respects LC_TIME. And does not support displaying iso-8601 format even if set en_DK
+        LC_TIME = "ja_JP.UTF-8";
+      };
+
       services.podman = {
         Unit = {
           Description = "Podman API Service";
