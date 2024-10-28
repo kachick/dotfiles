863447c1d6093ff57e04e69cdb1d0abd6695a708
Kenichi Kamiya <kachick1@gmail.com>
2024-10-28 13:47:50 +0900

    Fix a typo

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -4,7 +4,7 @@
     enable = true;
     # Settings keyremap in raw layer than X. See GH-784
     #
-    # -  Specify hardware names even if `evdev:input:*` working for mostcase. I should care both US and JIS layout
+    # - Specify hardware names even if `evdev:input:*` working for mostcase. I should care both US and JIS layout
     # - How to get the KEYBOARD_KEY_700??: `evtest /dev/input/event??`
     # - How to get the hardware name:: `udevadm info --attribute-walk /dev/input/event?? | grep -F 'ATTRS{name}'`
     # - How to apply?: Rebuild and reboot. Don't use `sudo systemd-hwdb update && sudo udevadm trigger` except debugging purpose.
