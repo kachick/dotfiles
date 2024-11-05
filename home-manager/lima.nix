bcf019555b979d186ab98b664eac107ae387df14
Kenichi Kamiya <kachick1@gmail.com>
2024-11-05 18:14:53 +0900

    Update lima default home directory

--- home-manager/lima.nix
+++ home-manager/lima.nix
@@ -1,5 +1,6 @@
 { config, ... }:
 
 {
-  home.homeDirectory = "/home/${config.home.username}.local";
+  # https://github.com/lima-vm/lima/blame/0d058b0eaa2d1bafc867298503a9239e89c202a8/templates/default.yaml#L295-L296
+  home.homeDirectory = "/home/${config.home.username}.linux";
 }
