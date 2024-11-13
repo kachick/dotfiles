fb3c7bc27eea1dd56c5a1d67a167ca665af77ee2
Kenichi Kamiya <kachick1@gmail.com>
2024-11-13 22:51:20 +0900

    Beautify

--- home-manager/genericLinux.nix
+++ home-manager/genericLinux.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
-
   # This also changes xdg? Official manual sed this config is better for non NixOS Linux
   # https://github.com/nix-community/home-manager/blob/559856748982588a9eda6bfb668450ebcf006ccd/modules/targets/generic-linux.nix#L16
   targets.genericLinux.enable = true;
@@ -13,5 +12,4 @@
       TERMINFO_DIRS = "${pkgs.kitty.terminfo}/share/terminfo";
     };
   };
-
 }
