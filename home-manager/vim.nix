9d018ca056f92b1872af06754f8f2269bd4537aa
Kenichi Kamiya <kachick1@gmail.com>
2024-11-16 18:35:04 +0900

    Adopt xdg.stateFile since 24.11

--- home-manager/vim.nix
+++ home-manager/vim.nix
@@ -5,8 +5,7 @@
 }:
 
 {
-  # TODO: Prefer xdg.stateFile since home-manager release-24.11. See https://github.com/nix-community/home-manager/pull/5779
-  home.file."${config.xdg.stateHome}/vim/.keep".text = "Keep this directory because of home-manager and vim does not create the file if directory is missing";
+  xdg.stateFile."vim/.keep".text = "Keep this directory because of home-manager and vim does not create the file if directory is missing";
 
   # https://github.com/nix-community/home-manager/blob/release-24.05/modules/programs/vim.nix
   # https://nixos.wiki/wiki/Vim
