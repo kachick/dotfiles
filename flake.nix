8bff93c179eddec9d1a900115819ed4b289f5e7e
Kenichi Kamiya <kachick1@gmail.com>
2024-11-19 23:31:07 +0900

    Revert "Backto NixOS 24.11 beta again"
    This reverts commit 00f14fc7dbcd1a6e6799ceca2d12c14343de7440.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
     # How to update the revision
     #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11"; # TODO: Pin to nixos-24.11 if https://github.com/NixOS/nixpkgs/pull/356590 applied
     edge-nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # Better than nixos-unstable until using darwin, however still not enough and might be broken. See https://github.com/NixOS/nixpkgs/issues/107466
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     # https://github.com/nix-community/home-manager/blob/release-24.11/docs/manual/nix-flakes.md
