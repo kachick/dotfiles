a4dc8c6dca9cc2be32cf961d8d1fe16fb85d3bb8
Kenichi Kamiya <kachick1@gmail.com>
2024-11-25 19:57:02 +0900

    [ci skip] Remove needless temporal code

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
       mkNixpkgs =
         system: if (nixpkgs.lib.strings.hasSuffix "-darwin" system) then nixpkgs-darwin else nixpkgs;
 
-      overlays = import ./overlays { inherit nixpkgs edge-nixpkgs; };
+      overlays = import ./overlays { inherit edge-nixpkgs; };
 
       mkPkgs =
         system:
