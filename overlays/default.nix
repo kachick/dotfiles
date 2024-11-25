2b0f70d5925bc347b9271ea680f4f5b459c7db25
Kenichi Kamiya <kachick1@gmail.com>
2024-11-25 18:50:52 +0900

    cloudflare-warp: 2024.9.346 -> 2024.11.309

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,7 @@
-{ edge-nixpkgs, ... }:
+{
+  edge-nixpkgs,
+  ...
+}:
 [
   (final: _prev: {
     my = import ../pkgs {
@@ -10,5 +13,25 @@
     unstable = import edge-nixpkgs {
       system = final.system;
     };
+  })
+
+  # Pacthed packages
+  (final: prev: {
+    cloudflare-warp = prev.cloudflare-warp.overrideAttrs (
+      finalAttrs: previousAttrs: rec {
+        version = "2024.11.309";
+
+        src = prev.fetchurl {
+          url = "https://pkg.cloudflareclient.com/pool/noble/main/c/cloudflare-warp/cloudflare-warp_${version}.0_${previousAttrs.suffix}.deb";
+          hash =
+            {
+              aarch64-linux = "sha256-pdCPN4NxaQqWNRPZY1CN03KnTdzl62vJ3JNfxGozI4k=";
+              x86_64-linux = "sha256-THxXETyy08rBmvghrc8HIQ2cBSLeNVl8SkD43CVY/tE=";
+            }
+            .${prev.stdenv.hostPlatform.system}
+              or (throw "Unsupported system: ${prev.stdenv.hostPlatform.system}");
+        };
+      }
+    );
   })
 ]
