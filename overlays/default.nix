415dbd2588b021a4a1e95433e50068d6e56f765b
Kenichi Kamiya <kachick1@gmail.com>
2024-12-01 16:29:45 +0900

    Backport https://github.com/NixOS/nixpkgs/commit/8b1b4b09c781e83de4fe6a53a81285d150e3780d

--- overlays/default.nix
+++ overlays/default.nix
@@ -42,24 +42,4 @@
         { }
     );
   })
-
-  # TODO: Remove after https://github.com/NixOS/nixpkgs/pull/358952 introduced in one of depending channels
-  (final: prev: {
-    cloudflare-warp = prev.cloudflare-warp.overrideAttrs (
-      finalAttrs: previousAttrs: rec {
-        version = "2024.11.309";
-
-        src = prev.fetchurl {
-          url = "https://pkg.cloudflareclient.com/pool/noble/main/c/cloudflare-warp/cloudflare-warp_${version}.0_${previousAttrs.suffix}.deb";
-          hash =
-            {
-              aarch64-linux = "sha256-pdCPN4NxaQqWNRPZY1CN03KnTdzl62vJ3JNfxGozI4k=";
-              x86_64-linux = "sha256-THxXETyy08rBmvghrc8HIQ2cBSLeNVl8SkD43CVY/tE=";
-            }
-            .${prev.stdenv.hostPlatform.system}
-              or (throw "Unsupported system: ${prev.stdenv.hostPlatform.system}");
-        };
-      }
-    );
-  })
 ]
