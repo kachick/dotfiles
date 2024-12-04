9e56c304d1c13536e5f3ae5934f9317192947595
Kenichi Kamiya <kachick1@gmail.com>
2024-12-04 19:58:50 +0900

    Remap katakanahiragawa with henkan to avoid my bug
    Similar probelm of GH-963 when using JIS keyboards

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -2,15 +2,17 @@
 {
   services.udev = {
     enable = true;
-    # Settings keyremap in raw layer than X. See GH-784 for background. And see https://github.com/kachick/dotfiles/wiki/Key-Remapper for the guide
+    # Settings keyremap in raw layer than X. See GH-784 and GH-963 for background. And see https://github.com/kachick/dotfiles/wiki/Key-Remapper for the guide
     #
-    # Specify hardware names even if `evdev:input:*` working for mostcase. I should care both US and JIS layout
+    # - Specify hardware names even if `evdev:input:*` working for mostcase. I should care both US and JIS layout
+    # - You can get the devicename, scancode and keycode with evtest. So kill xremap and run `evtest` with no arguments. Interactively choose the device
     extraHwdb = lib.mkBefore ''
       evdev:name:Topre REALFORCE 87 US:*
         KEYBOARD_KEY_70039=leftctrl # original: capslock
 
       evdev:name:Lenovo ThinkPad Compact USB Keyboard with TrackPoint:* # Both US and JIS have same name
         KEYBOARD_KEY_70039=leftctrl # original: capslock, Both US and JIS have same keycode for capslock
+        KEYBOARD_KEY_70088=henkan # original: KATAKANAHIRAGANA, guessing US does not have this code, fix me with specifying vendor and product id if required
 
       evdev:name:Lenovo Wireless KB Keyboard:* # JIS
         KEYBOARD_KEY_70039=leftctrl # original: capslock
