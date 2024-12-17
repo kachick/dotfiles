5e2b3b8fa8eb87dda76932813dd229ce044132ba
Kenichi Kamiya <kachick1@gmail.com>
2024-12-17 13:13:36 +0900

    Fix kanata package and nix config

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  inputs,
   pkgs,
   lib,
   ...
--- nixos/desktop/kanata.nix
+++ nixos/desktop/kanata.nix
@@ -1,9 +1,12 @@
+{ pkgs, ... }:
 {
   # Left Control + Space + Esc: Force exit
   #
   # https://github.com/NixOS/nixpkgs/blob/nixos-24.11/nixos/modules/services/hardware/kanata.nix
   services.kanata = {
     enable = true;
+
+    package = pkgs.unstable.kanata; # TODO: Use stable since nixos-25.05. nixos-24.11 intentionally keeps 1.7.0-pre version
 
     keyboards = {
       # Intentionally setting alt to henkan and muhenkan even in JIS layouts to consider my mis-typing
@@ -13,7 +16,6 @@
           (deflayermap (base)
             caps (tap-hold-press 200 200 caps lctl)
             lalt (tap-hold-press 200 200 lalt muhenkan)
-            ralt (tap-hold-press 200 200 ralt henkan)
           )
         '';
       };
