dde5f3c8ad74814137d41947c0172f687073eb95
Kenichi Kamiya <kachick1@gmail.com>
2024-12-17 18:52:08 +0900

    Refine comments

--- nixos/desktop/kanata.nix
+++ nixos/desktop/kanata.nix
@@ -6,10 +6,13 @@
   services.kanata = {
     enable = true;
 
-    package = pkgs.unstable.kanata; # TODO: Use stable since nixos-25.05. nixos-24.11 intentionally keeps 1.7.0-pre version
-
+    # nixos-24.11 intentionally keeps 1.7.0-pre version
+    # https://github.com/NixOS/nixpkgs/pull/354894
+    # https://github.com/NixOS/nixpkgs/pull/351675#issuecomment-2440047546
+    package = pkgs.unstable.kanata; # TODO: Use stable since nixos-25.05
     keyboards = {
       # Intentionally setting alt to henkan and muhenkan even in JIS layouts to consider my mis-typing
+      # tap-hold-press: tap-next in kmonad. See https://github.com/jtroo/kanata/issues/7#issuecomment-1196236726
       all = {
         config = ''
           ;; Required to specify keys which should be considered in tap-hold-press (=spc, tab for alt-spc, alt-tab)
