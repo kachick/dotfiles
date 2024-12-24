b2095c8915b9f457d39b24a32fa54874adc8b42d
Kenichi Kamiya <kachick1@gmail.com>
2024-12-24 22:53:30 +0900

    Ensure CGO_ENABLED=0

--- pkgs/reponame/package.nix
+++ pkgs/reponame/package.nix
@@ -6,6 +6,10 @@ pkgs.buildGo123Module rec {
   vendorHash = "sha256-1wycFQdf6sudxnH10xNz1bppRDCQjCz33n+ugP74SdQ=";
   src = ./.;
 
+  # https://github.com/kachick/times_kachick/issues/316
+  # TODO: Use env after nixos-25.05. See https://github.com/NixOS/nixpkgs/commit/905dc8d978b38b0439905cb5cd1faf79163e1f14#diff-b07c2e878ff713081760cd5dcf0b53bb98ee59515a22e6007cc3d974e404b220R24
+  CGO_ENABLED = 0;
+
   meta = {
     description = "kachick/dotfiles => dotfiles, dotfiles => dotfiles";
     mainProgram = pname;
