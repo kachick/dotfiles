187725569805c5457607f76543516549156aa9b1
Kenichi Kamiya <kachick1@gmail.com>
2024-12-30 18:47:24 +0900

    Add ghostty terminfo in containers and lima

--- home-manager/genericLinux.nix
+++ home-manager/genericLinux.nix
@@ -9,7 +9,7 @@
       # Make it possible to handle "xterm-kitty" in SSH remotes or lima guest VM with tiny filesize and setups. See GH-932
       #
       # Don't set this in NixOS desktop. It has own value.
-      TERMINFO_DIRS = "${pkgs.kitty.terminfo}/share/terminfo";
+      TERMINFO_DIRS = "${pkgs.kitty.terminfo}/share/terminfo:${pkgs.unstable.ghostty.terminfo}/share/terminfo";
     };
   };
 }
