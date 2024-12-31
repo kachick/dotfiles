5aa18b4c26f9e9431380ea4a09c5a9699a245066
Kenichi Kamiya <kachick1@gmail.com>
2024-12-31 17:26:42 +0900

    Consider -o option

--- pkgs/git-hooks-pre-push/pre-push.bash
+++ pkgs/git-hooks-pre-push/pre-push.bash
@@ -1,8 +1,12 @@
+# Avoiding -o error: https://stackoverflow.com/a/7832158
+# This is an escape hatch for large repository
+DO_HOOK=${RUN_GITHOOK_HOG:-true}
+
 # list of arguments: https://git-scm.com/docs/githooks#_pre_push
 while read -r local_ref _local_oid remote_ref _remote_oid; do
 	# - trufflehog pre-commit hook having crucial limitations. https://github.com/trufflesecurity/trufflehog/blob/v3.88.0/README.md?plain=1#L628-L629
 	# - Adding `--since-commit main` made 10x slower... :<
-	if [[ "$ENABLE_GITHOOK_HOG" != "false" ]]; then
+	if [[ "$DO_HOOK" != "false" ]]; then
 		trufflehog git "file://${PWD}" --results='verified,unknown' --branch "$local_ref" --fail
 	fi
 
