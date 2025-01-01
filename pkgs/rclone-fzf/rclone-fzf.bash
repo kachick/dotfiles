1705f4fc108e5309dc75618bf076b7d865d7ff37
Kenichi Kamiya <kachick1@gmail.com>
2025-01-01 19:22:30 +0900

    Add columns into rclone-list-mouted

--- pkgs/rclone-fzf/rclone-fzf.bash
+++ pkgs/rclone-fzf/rclone-fzf.bash
@@ -16,13 +16,15 @@ rclone-mount() {
 
 export -f rclone-mount
 
+# Don't use --preview for this. It omits some columns, and this will not be changed with selected entry
+rclone-list-mounted >&2
+
 # Don't use rclone in fzf --preview, it runs query and be slow
 rclone listremotes | fzf --query "$query" --border-label '☁️ Rclone Remotes' \
-	--layout=reverse --multi --height=85% --min-height=20 --border \
+	--layout=reverse --height=85% --min-height=20 --border \
 	--border-label-pos=2 \
 	--color='header:italic:underline,label:blue' \
 	--header $'Alt-M (Mount) / Alt-A (Inspect)\n\n' \
-	--preview 'rclone-list-mounted' \
 	--bind 'alt-m:become(rclone-mount {})' \
 	--bind 'alt-a:become(rclone about {})' \
 	--bind 'enter:become(echo {})'
--- pkgs/rclone-list-mounted/script.nu
+++ pkgs/rclone-list-mounted/script.nu
@@ -1,1 +1,9 @@
-mount --types fuse.rclone | lines | split column ' ' | rename remote _ local _ type | select remote local | sort-by remote
+def list-rclone-mount-includes-garbage [] {
+  mount --types fuse.rclone | lines | split column ' ' | rename remote _ local _ type | select remote local | sort-by remote
+}
+
+def list-rclone-process [] {
+  ps --long | where name =~ rclone and command =~ mount | select pid ppid command | insert local { |row| $row.command | parse --regex '\A\S+rclone\S+ mount \S+?: (?P<local>\S+)' | first | get local | into string }
+}
+
+list-rclone-mount-includes-garbage | join --left (list-rclone-process) local | reject command
