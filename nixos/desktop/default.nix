76958331bb2883e61b15bcd77a01bd5915bc6878
Kenichi Kamiya <kachick1@gmail.com>
2025-01-10 13:44:14 +0900

    Revert "Set default TERMINAL with ghostty for GNOME switcher"
    This reverts commit 71a447b153eae3663c4c739a623850bd0e37d6d5.

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -283,8 +283,6 @@
     XCURSOR_THEME = "Adwaita";
 
     __HM_SESS_VARS_SOURCED = ""; # Workaround for GH-755 and GH-890
-
-    TERMINAL = lib.getExe pkgs.unstable.ghostty;
   };
 
   # https://github.com/NixOS/nixpkgs/issues/33282#issuecomment-523572259
