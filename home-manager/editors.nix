f6fb452941e55567cc82cdd6f5ba00f56112180f
Kenichi Kamiya <kachick1@gmail.com>
2025-01-21 18:43:28 +0900

    Manage ox config by home-manager

--- home-manager/editors.nix
+++ home-manager/editors.nix
@@ -29,5 +29,10 @@
         sha256 = "sha256-F8xP4xCIS1ybvRm1xGB2USekGWKKxz0nokpY6gRxKBE=";
       }
       + "/.editorconfig";
+
+    # - Ideally want to put to `$XDG_CONFIG_HOME/ox/config.ext`, however it only be used for storing plugins
+    # - $XDG_CONFIG_HOME/.oxrc didn't work even if written in the 0.7.6 usage
+    # - $XDG_CONFIG_HOME looks like a hardcoded, it might be broken if I change the value from default - https://github.com/curlpipe/ox/blob/4a2ef2521c983d67c6aaf0796673a73791fdca95/src/plugin/plugin_manager.lua#L121
+    file.".oxrc".source = ../config/ox/.oxrc;
   };
 }
