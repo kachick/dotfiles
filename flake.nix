6d43643b6d7b0d36b8306ee2378aa7c2061fba35
Kenichi Kamiya <kachick1@gmail.com>
2025-01-27 20:56:25 +0900

    Install go-task in devshell

--- flake.nix
+++ flake.nix
@@ -103,6 +103,7 @@
                 # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
                 bashInteractive
                 cargo-make
+                go-task
               ])
               ++ (pkgs.lib.optionals pkgs.stdenv.isLinux (
                 (with pkgs; [
