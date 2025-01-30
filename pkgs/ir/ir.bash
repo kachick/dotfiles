ecffc983fc7a57161d82813226871460805a8597
Kenichi Kamiya <kachick1@gmail.com>
2025-01-30 19:51:54 +0900

    Refine inline replacer with sad
    Update e1bf74a73df4cf6739964254e5d53ef8de7f952a

--- pkgs/ir/ir.bash
+++ pkgs/ir/ir.bash
@@ -1,13 +1,21 @@
-tmprb="$(mktemp --suffix='.rb')"
-cat <<'RUBY' >"$tmprb"
-$_.gsub!(
-  /before/,
-  %q|after|
-)
-RUBY
-command "$EDITOR" "$tmprb"
+if [ $# -lt 2 ]; then
+	# shellcheck disable=SC2016
+	echo 'Should be specified $1:<PATTERN> and $2:[REPLACE]' >&2
+fi
 
-# TODO: grep takes the "before" if given then via CLI
-fd --type file --hidden --exclude .git |
-	xargs grep --binary-files=without-match -l --fixed-strings -e '' |
-	xargs ruby -i -pn "$tmprb"
+# https://stackoverflow.com/a/28090709
+nl=$'\n'
+
+fzf_options="$(
+	cat <<-EOF
+		--style full --height 80% --layout reverse \
+		--border --padding '1,2' --border-label ' Inline Replacer ' \
+		--input-label ' Path Filter ' \
+		--header-label ' Keybinds ' \
+		--header "Tab(+Shift): Toggle${nl}Alt+t: Toggle All" \
+		--bind 'alt-t:toggle-all'
+	EOF
+)"
+
+fd --type file --hidden --exclude .git --exclude .direnv --exclude node_modules |
+	sad --fzf="$fzf_options" "$@"
