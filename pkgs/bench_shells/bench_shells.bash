3961a2b95472e695ede2657738fbde1e7349e07a
Kenichi Kamiya <kachick1@gmail.com>
2025-02-05 10:29:21 +0900

    Make bench_shells can take optional arguments to delegrate hyperfine
    Such as `bench_shell --export-markdown -`

--- pkgs/bench_shells/bench_shells.bash
+++ pkgs/bench_shells/bench_shells.bash
@@ -1,5 +1,5 @@
 # zsh should be first, because it often makes much slower with the completion
-hyperfine --warmup 1 --runs 3 --shell none \
+hyperfine --warmup 1 --runs 3 --shell none "$@" \
 	'zsh --interactive -c exit' \
 	'zsh --no-rcs --interactive -c exit' \
 	'bash -i -c exit' \
