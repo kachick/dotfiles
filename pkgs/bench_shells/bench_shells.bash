860ec33bfae1a46e8344db25b7d9a9c84d5cf4bf
Kenichi Kamiya <kachick1@gmail.com>
2025-02-05 10:21:50 +0900

    Compare plain shells in benchmarking

--- pkgs/bench_shells/bench_shells.bash
+++ pkgs/bench_shells/bench_shells.bash
@@ -1,5 +1,8 @@
 # zsh should be first, because it often makes much slower with the completion
-hyperfine --warmup 1 --runs 5 \
+hyperfine --warmup 1 --runs 3 --shell none \
 	'zsh --interactive -c exit' \
+	'zsh --no-rcs --interactive -c exit' \
 	'bash -i -c exit' \
-	'nu --interactive --commands exit'
+	'bash --norc -i -c exit' \
+	'nu --interactive --commands exit' \
+	'nu --no-config-file --interactive --commands exit'
