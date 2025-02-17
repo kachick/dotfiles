0c6663f16b7c5971929a5f06c710e17864a09f5a
Kenichi Kamiya <kachick1@gmail.com>
2025-02-18 01:30:35 +0900

    Fix implementation for multilines

--- pkgs/fzf-bind-posix-shell-history-to-git-commit-message/fzf-bind-posix-shell-history-to-git-commit-message.bash
+++ pkgs/fzf-bind-posix-shell-history-to-git-commit-message/fzf-bind-posix-shell-history-to-git-commit-message.bash
@@ -3,7 +3,7 @@
 # - lstrip is not enough for some history
 # - Keep line-end in fzf input
 # shellcheck disable=SC2016 disable=SC2086
-nu --stdin --commands 'str trim' |
+nu --stdin --commands 'lines | each { str trim } | to text' |
 	fzf --height ''${FZF_TMUX_HEIGHT:-40%} ''${FZF_DEFAULT_OPTS-} \
 		-n2..,.. --scheme=history \
 		--bind 'enter:become(safe_quote_backtik {} | git commit -a -F -)'
