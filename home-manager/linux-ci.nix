3ab920b3408f218a54c2b6f4ffaf3c937cc6f948
Kenichi Kamiya <kachick1@gmail.com>
2025-02-18 11:10:20 +0900

    Add CI for unstable heavy packages and reverts some flake.lock updates to fix broken zed-editor and nodejs build on NixOS (#1086)
    * Add CI for several unstable packages before merging
    
    * Revert "flake.lock: Update (#1084)"
    
    This reverts commit bf0dfdf0b8a1ec3e0491c624b0d2fd99d7390560.
    
    * Revert "flake.lock: Update (#1081)"
    
    This reverts commit a5499bc8c2d32aae86fdf03331968e82f8335f1a.
    
    * Revert "Bump flake.lock and related dependencies (#1079)"
    
    This reverts commit 3351d379dbd46a5173a9b13a75580f28ae4f1036.
    
    * Revert "flake.lock: Update (#1077)"
    
    This reverts commit 0c4f28306ead24f09dfc9513c340929230af053f.
    
    * Extract windows note into wiki
    
    https://github.com/kachick/dotfiles/wiki/Windows
    
    Avoiding a lychee error is a big reason https://github.com/kachick/dotfiles/pull/1086/files#r1958955810

---
+++ home-manager/linux-ci.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  ...
+}:
+
+# This file is provided for CI use.
+# Especially for unstable and heavy building packages for NixOS desktop.
+# I don't need them on my container, however not checking in CI often crushes my desktop experience.
+# So ensure the CI result before merging updates.
+# Providing since GH-1085. It migtht be superseded by GH-642.
+
+{
+  home = {
+    packages = with pkgs; [
+      unstable.zed-editor
+    ];
+  };
+}
