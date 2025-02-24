1a8d9db9f891da8efe3b2fe3dbb49559176871ba
Kenichi Kamiya <kachick1@gmail.com>
2025-02-25 02:22:24 +0900

    Trigger home-manager CI when go.mod is  changed in pkgs (#1096)
    * Trigger home-manager CI when changed go.mod in pkgs
    
    Noticed in https://github.com/kachick/dotfiles/pull/1095#issuecomment-2679144394
    
    * Bump vendorHash in reponame

--- pkgs/reponame/package.nix
+++ pkgs/reponame/package.nix
@@ -3,7 +3,7 @@ pkgs.unstable.buildGo124Module rec {
   pname = "reponame";
   version = "0.0.1";
   default = pname;
-  vendorHash = "sha256-1wycFQdf6sudxnH10xNz1bppRDCQjCz33n+ugP74SdQ=";
+  vendorHash = "sha256-uPqabZgQGQulf+F3BvMLhv4O0h5jOq12F7K60u5xjtA=";
   src = ./.;
 
   env.CGO_ENABLED = 0;
