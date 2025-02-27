8bc4f2807cb0a469356e354d3455251638424f2e
Kenichi Kamiya <kachick1@gmail.com>
2025-02-27 12:11:24 +0900

    Attempt to fix devshell CI
    Susupecting is is same problem as 904b2b91ad9868bc435ed5bd2c1f249a9759c973

--- cmd/lint/main.go
+++ cmd/lint/main.go
@@ -13,6 +13,20 @@ import (
 	"github.com/kachick/dotfiles/internal/runner"
 )
 
+func getExhaustructPath() string {
+	exhaustructResult, err := exec.Command("go", []string{"tool", "-n", "exhaustruct"}...).CombinedOutput()
+	if err != nil {
+		log.Fatalf("Missing exhaustruct as a vettool: %+v", err)
+	}
+
+	// It downloads dependencies and outputs them in first run. So getting only last line here such as tail command
+	var last string
+	for line := range strings.Lines(string(exhaustructResult)) {
+		last = line
+	}
+	return strings.TrimSpace(last)
+}
+
 func main() {
 	allFlag := flag.Bool("all", false, "includes heavy linters")
 
@@ -23,17 +37,11 @@ func main() {
 	bashPaths := walker.GetAllBash()
 	markdownPaths := walker.GetAllMarkdown()
 
-	rawExhaustructPath, err := exec.Command("go", []string{"tool", "-n", "exhaustruct"}...).CombinedOutput()
-	if err != nil {
-		log.Fatalf("Missing exhaustruct as a vettool: %+v", err)
-	}
-	exhaustructPath := strings.TrimSpace(string(rawExhaustructPath))
-
 	// Don't add secrets scanner here. It should be done in pre-push hook now.
 	cmds := runner.Commands{
 		{Path: "shellcheck", Args: bashPaths},
 		{Path: "typos", Args: constants.GetTyposTargetedRoots()},
-		{Path: "go", Args: []string{"vet", "-vettool", exhaustructPath, "./..."}},
+		{Path: "go", Args: []string{"vet", "-vettool", getExhaustructPath(), "./..."}},
 		{Path: "nixpkgs-lint", Args: []string{"."}},
 		{Path: "markdownlint-cli2", Args: markdownPaths},
 		// Add selfup as `git ls-files | xargs selfup list -check`. Consider https://github.com/kachick/dotfiles/issues/905 for use of pipe
