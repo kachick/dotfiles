2508b7aac0ae0063d29470b76979e60fe874a7ca
Kenichi Kamiya <kachick1@gmail.com>
2025-03-14 21:02:31 +0900

    Fix typo more

--- pkgs/toggle-wofi/toggle-wofi.bash
+++ pkgs/toggle-wofi/toggle-wofi.bash
@@ -1,5 +1,5 @@
 # Anti-pattern:
-#   - No option: It might match for this script itself. For pname. And Nis store path is much longer
+#   - No option: It might match for this script itself. For pname. And Nix long store paths make me afraid even through they are just hash strings
 #   - --exact: It only support less than 15 chars
 #   - Injecting full path with nix: It is required in both killing and executing. And might be kept in updating.
 # Depending the arguments is bit hacky, however it is easy and okay for now. I may replace this implementation with `pkill --pidfile`
