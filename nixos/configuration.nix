a6f5c0cd09c08649db03e27efefb5ff22c3647f3
Kenichi Kamiya <kachick1@gmail.com>
2025-03-21 02:01:21 +0900

    Set hosts specific WiFi interface-name

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,10 +42,15 @@
     # Because of only using networkmanager or using lwd for backend made unstable WiFi on laptop
     # Then it made much of these logs
     # wlp3s0: Limiting TX power to 30 (30 - 0) dBm as advertised by ...
+    #
     # You can get the interface-name name with `iw dev`. ref: https://wiki.archlinux.org/title/Network_configuration/Wireless#Get_the_name_of_the_interface
-    unmanaged = [
-      "except:interface-name:wlp3s0"
-    ];
+    # Set it in each host.
+    #
+    # For example
+    #
+    # unmanaged = [
+    #   "except:interface-name:wlp3s0"
+    # ];
   };
 
   # TODO: Reconsider to set UTC for servers
