0776e2820674bf604c64b04ced72767fc45ea900
Kenichi Kamiya <kachick1@gmail.com>
2025-03-21 23:16:16 +0900

    How about adding pseudo values?

--- nixos/hosts/nixos/default.nix
+++ nixos/hosts/nixos/default.nix
@@ -22,4 +22,20 @@
     # https://discourse.nixos.org/t/no-space-left-on-boot/24019/20
     configurationLimit = 10;
   };
+
+  # Pseudo values to pass flake check validations
+  # You should override in your hardware-configuration.nix
+
+  fileSystems."/" = lib.mkDefault {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  networking.networkmanager = lib.mkDefault {
+    unmanaged = [
+    ];
+  };
+
 }
