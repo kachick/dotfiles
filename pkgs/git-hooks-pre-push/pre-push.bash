42402ea228f47b5da910b7664f54a023649f30d8
Kenichi Kamiya <kachick1@gmail.com>
2025-03-21 17:36:23 +0900

    Scan secrets with remote branch instead of local (#1131)
    * Add missing git in the dependency
    
    * Scan secrets for remote branch instead of local
    
    Now not working for main to main, this will fix it

--- pkgs/git-hooks-pre-push/pre-push.bash
+++ pkgs/git-hooks-pre-push/pre-push.bash
@@ -4,13 +4,13 @@
 # Copying to another env is avoiding -o error: https://stackoverflow.com/a/7832158
 SKIP_HOOK=${SKIP:-}
 
-DEFAULT_BRANCH="$(basename "$(git symbolic-ref 'refs/remotes/origin/HEAD')")"
+REMOTE_DEFAULT_BRANCH="$(git symbolic-ref 'refs/remotes/origin/HEAD')"
 
 # list of arguments: https://git-scm.com/docs/githooks#_pre_push
 while read -r local_ref _local_oid remote_ref _remote_oid; do
 	if [[ "$SKIP_HOOK" != "gitleaks" ]]; then
 		# TODO: Might be better to skip if author is another person in nixpkgs
-		gitleaks git --log-opts="$DEFAULT_BRANCH..$local_ref"
+		gitleaks git --log-opts="$REMOTE_DEFAULT_BRANCH..$local_ref"
 	fi
 
 	# Git ref is not a file path, but avoiding a typos limitation for slash
