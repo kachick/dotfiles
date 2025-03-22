6ec5772b6e9b91f4ebb0951487b4180802dd4a60
Kenichi Kamiya <kachick1@gmail.com>
2025-03-23 00:15:04 +0900

    Enable RTW89 first

--- nixos/hosts/moss/default.nix
+++ nixos/hosts/moss/default.nix
@@ -22,7 +22,9 @@
       # https://github.com/torvalds/linux/blob/88d324e69ea9f3ae1c1905ea75d717c08bdb8e15/drivers/net/wireless/realtek/rtw89/Kconfig#L87-L96
       name = "enable-rtw89_8852ce-config";
       patch = null;
+      # Also requires RTW89 first. https://github.com/NixOS/nixpkgs/issues/82951#issuecomment-602031597
       extraConfig = ''
+        RTW89 y
         RTW89_8852CE y
       '';
     }
