d299c71c7ab65d7dde8c5f48beb0d8581bbcf63a
Kenichi Kamiya <kachick1@gmail.com>
2025-03-27 15:46:54 +0900

    Apply same SKIP feature in commit-msg hook
    Might be better with following
    
    - Translate to Go
    - Share ENV handling

--- pkgs/git-hooks-commit-msg/commit-msg.bash
+++ pkgs/git-hooks-commit-msg/commit-msg.bash
@@ -1,4 +1,15 @@
-typos --config "$TYPOS_CONFIG_PATH" "$1"
-gitleaks --verbose stdin <"$1"
+declare -A SKIP_HOOKS=([gitleaks]='false' [typos]='false')
+while IFS=, read -r target; do
+	[ -z "$target" ] && continue
+	SKIP_HOOKS["$target"]='true'
+done < <(echo "${SKIP:-}")
+
+if [[ "${SKIP_HOOKS[typos]}" != 'true' ]]; then
+	typos --config "$TYPOS_CONFIG_PATH" "$1"
+fi
+
+if [[ "${SKIP_HOOKS[gitleaks]}" != 'true' ]]; then
+	gitleaks --verbose stdin <"$1"
+fi
 
 run_local_hook 'commit-msg' "$@"
