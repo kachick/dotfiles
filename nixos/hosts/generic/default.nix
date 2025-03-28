8b0a1f454d599af77fa89cd0efd4cc1f1f705daa
Kenichi Kamiya <kachick1@gmail.com>
2025-03-28 16:02:16 +0900

    Fix missing basic configuration.nix

--- nixos/hosts/generic/default.nix
+++ nixos/hosts/generic/default.nix
@@ -4,17 +4,12 @@
   networking.hostName = lib.mkDefault "nixos";
 
   imports = [
-    # ./hardware-configuration.nix # Comment-in only in your new device
+    ../../configuration.nix
     ../../hardware.nix
     ../../desktop
     ../../desktop/genericUsers.nix
 
-    # You should copy flake.example.nix in your /etc/nixos and apply it with pure mode
-    # This is why:
-    # - Don't save the hardware-configuration.nix in this repository for abstracted use-case in several devices even after GH-712.
-    # - Don't refer /etc/nixos/*.nix  here, it requires impure mode
-    # - Impure mode is annoy, when including it, flake check will fail
-    # - We can't partially run flake check. See https://github.com/NixOS/nix/issues/8881
+    # ./hardware-configuration.nix # Comment-in only in your new device
   ];
 
   boot.loader.systemd-boot = {
