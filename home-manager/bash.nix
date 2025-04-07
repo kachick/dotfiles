9e4d3417a47d8e9ac822edd76cde6eebb5fc6329
Your Name <you@example.com>
2025-04-07 13:34:22 +0900

    Reapply "This hack might be guilty"
    This reverts commit fd4773b293dc64d856cd81e989bd4e02ea930812.

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -93,15 +93,15 @@
     ];
 
     # Switch to another shell when bash used as a login shell
-    profileExtra = ''
-      # Used same method as switching to fish
-      # https://wiki.archlinux.org/title/fish#Setting_fish_as_interactive_shell_only
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.zsh}/bin/zsh $LOGIN_OPTION
-      fi
-    '';
+    # profileExtra = ''
+    #   # Used same method as switching to fish
+    #   # https://wiki.archlinux.org/title/fish#Setting_fish_as_interactive_shell_only
+    #   if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ''${BASH_EXECUTION_STRING} ]]
+    #   then
+    #     shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+    #     exec ${pkgs.zsh}/bin/zsh $LOGIN_OPTION
+    #   fi
+    # '';
 
     # For interactive shells. In .bashrc and after early return
     # https://github.com/nix-community/home-manager/blob/release-24.11/modules/programs/bash.nix#L221-L222
