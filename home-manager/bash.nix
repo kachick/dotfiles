c463ced59945794101e57929348b98b40dd06049
Kenichi Kamiya <kachick1@gmail.com>
2025-04-07 19:40:41 +0900

    Consider $SHLVL in shell delegations
    Follow https://wiki.archlinux.org/index.php?title=Fish&diff=prev&oldid=811412

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -96,7 +96,7 @@
     profileExtra = ''
       # Used same method as switching to fish
       # https://wiki.archlinux.org/title/fish#Setting_fish_as_interactive_shell_only
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ''${BASH_EXECUTION_STRING} ]]
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ''${BASH_EXECUTION_STRING} && ''${SHLVL} == 1  ]]
       then
         shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
         exec ${pkgs.zsh}/bin/zsh $LOGIN_OPTION
