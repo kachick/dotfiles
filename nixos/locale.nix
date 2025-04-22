a07178bcb2eec2f6d28561f2d650d049e2fd4e85
Kenichi Kamiya <kachick1@gmail.com>
2025-04-23 02:37:01 +0900

    Fix locale error when using en_DK

--- nixos/locale.nix
+++ nixos/locale.nix
@@ -6,6 +6,14 @@
     # GNOME respects this, I don't know how to realize it only via home-manager
     defaultLocale = "ja_JP.UTF-8";
 
+    # Candidates: https://sourceware.org/git/?p=glibc.git;a=blob;f=localedata/SUPPORTED
+    supportedLocales = [
+      "C.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
+      "en_DK.UTF-8/UTF-8"
+    ];
+
     extraLocaleSettings = {
       # FIXME: Don't set LC_TIME here, it makes strange and unstable behaviors. Correctly overridable in user systemd on algae and not working in moss. Even if both device have almost same config...
       # https://wiki.archlinux.jp/index.php/%E3%83%AD%E3%82%B1%E3%83%BC%E3%83%AB
