0dc7e5f2a95edaa61b3fdcf0f2c79e2d192f76f0
Kenichi Kamiya <kachick1@gmail.com>
2025-05-17 03:25:01 +0900

    Hmm

--- flake.nix
+++ flake.nix
@@ -155,7 +155,7 @@
       apps = forAllSystems (system: {
         home-manager = mkApp {
           inherit system;
-          pkg = (mkHomeManager system).packages.${system};
+          pkg = (mkHomeManager system).packages.${system}.home-manager;
         };
       });
 
