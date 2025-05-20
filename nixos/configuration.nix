bd03e899cbe5a016310a1a2cecefe4e0e1c843ce
Kenichi Kamiya <kachick1@gmail.com>
2025-05-20 12:47:56 +0900

    Enable nix optimization and GC to free up disk space
    Especially algae is always hungry, 1TB disk is too small for NixOS desktop

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,6 +18,19 @@
     "flakes"
   ];
 
+  nix.optimise.automatic = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  nix.extraOptions = ''
+    min-free = ${toString (100 * 1024 * 1024)}
+    max-free = ${toString (1024 * 1024 * 1024)}
+  '';
+
   # Bootloader.
   boot.loader.efi.canTouchEfiVariables = true;
 
