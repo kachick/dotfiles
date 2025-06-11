15fe2f0be2c27a210fbd575f22e802c120811710
Kenichi Kamiya <kachick1@gmail.com>
2025-06-11 18:30:13 +0900

    Fix a typo

--- overlays/default.nix
+++ overlays/default.nix
@@ -38,7 +38,7 @@
     patched = {
       # Based on forked version of lima, because of the extracting addiitional guest agents made much faster to build.
       # Mandatory if no binary cache such as this patch
-      # (It will make slower building on macOS, because they don't need need this ssh patch. However testing with latest is better for now.)
+      # (It will make slower building on macOS, because they don't need this ssh patch. However testing with latest is better for now.)
       # ref: https://github.com/NixOS/nixpkgs/pull/415093
       lima = prev.my.lima.overrideAttrs (
         finalAttrs: previousAttrs:
