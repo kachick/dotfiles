93d245e05434ccf59294a22aa2d807a7de79aaa0
Kenichi Kamiya <kachick1@gmail.com>
2025-06-14 21:25:59 +0900

    Remove shogihome workaround since backporting nixpkgs patch
    GH-1199 included https://github.com/NixOS/nixpkgs/commit/c73df248ae10a095f6de8288d805508ce57997cc
    So updates cbe92fba8f8cf86f5e3d52f75a4b7e16a5092598

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -195,27 +195,11 @@
 
       # shogihome does not provide configuration schema and ENV, so manually setup the foollowing NNUE evaluation files for the engine
       # Related issue: https://github.com/sunfish-shogi/shogihome/issues/1017
-      (
-        (unstable.shogihome.override {
-          commandLineArgs = [
-            "--wayland-text-input-version=3"
-          ];
-        }).overrideAttrs
-        (prevAttrs: {
-          desktopItems = [
-            (makeDesktopItem {
-              name = "shogihome";
-              exec = "shogihome %U";
-              icon = "shogihome";
-              desktopName = "ShogiHome";
-              genericName = "Shogi Frontend";
-              comment = prevAttrs.meta.description;
-              categories = [ "Game" ];
-              startupWMClass = "electron-shogi"; # Fixed from nixpkgs
-            })
-          ];
-        })
-      )
+      (unstable.shogihome.override {
+        commandLineArgs = [
+          "--wayland-text-input-version=3"
+        ];
+      })
 
       my.tanuki-hao # NNUE evaluation file. It put under /run/current-system/sw/share/eval
 
