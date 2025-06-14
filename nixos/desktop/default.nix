1befbf93ade4ba040bf607461a9be370749197e8
Kenichi Kamiya <kachick1@gmail.com>
2025-06-14 22:21:46 +0900

    Fix gnome-boxes missing KVM errors

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -8,6 +8,7 @@
 {
   imports = [
     (import ./font.nix { inherit pkgs; })
+    (import ./vm.nix { inherit pkgs; })
     ./kanata.nix
   ];
 
@@ -119,21 +120,6 @@
       ghostty # ghostty package now always be backported.
 
       alacritty
-
-      # Ensure existing qemu-img with lima for use of systemd.
-      # Because of lima might be started with systemd, and then the Nix wrapped qemu PATH will be ignored.
-      # See GH-1049 for detail.
-      qemu
-
-      # Why not other candidates?
-      #
-      # * cockpit: Might be the primary candidate if cockpit-machines is also available. ref: https://github.com/NixOS/nixpkgs/issues/287644
-      # * virt-manager: It seems too much for my use case. However it is still a candidate.
-      # * quickemu: Having another definitions and release cycle than osinfo-db. And seems not actively maintained in these days.
-      #
-      # Use latest to apply latest osinfo-db such as https://github.com/nixos/nixpkgs/pull/414620
-      # It is the actual OS information for the VM, upstream is https://gitlab.com/libosinfo/osinfo-db
-      unstable.gnome-boxes
 
       lapce # IME is not working on Windows, but stable even around IME on Wayland than vscode
 
---
+++ nixos/desktop/vm.nix
@@ -0,0 +1,29 @@
+# This file defines GUI VM management on NixOS desktop. See also lima related files in home-manager
+
+{ pkgs, ... }:
+{
+  # programs.virt-manager.enable = true;
+
+  users.groups.libvirtd.members = [ "kachick" ];
+
+  virtualisation.libvirtd.enable = true;
+
+  virtualisation.spiceUSBRedirection.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    # Ensure existing qemu-img with lima for use of systemd.
+    # Because of lima might be started with systemd, and then the Nix wrapped qemu PATH will be ignored.
+    # See GH-1049 for detail.
+    qemu
+
+    # Why not other candidates?
+    #
+    # * cockpit: Might be the primary candidate if cockpit-machines is also available. ref: https://github.com/NixOS/nixpkgs/issues/287644
+    # * virt-manager: It seems too much for my use case. However it is still a candidate.
+    # * quickemu: Having another definitions and release cycle than osinfo-db. And seems not actively maintained in these days.
+    #
+    # Use latest to apply latest osinfo-db such as https://github.com/nixos/nixpkgs/pull/414620
+    # It is the actual OS information for the VM, upstream is https://gitlab.com/libosinfo/osinfo-db
+    unstable.gnome-boxes
+  ];
+}
