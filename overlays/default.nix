2e2880abb6d2a21cb0381dd5f8ace23c6dc1a58f
Kenichi Kamiya <kachick1@gmail.com>
2025-06-19 15:57:23 +0900

    Update lima patch

--- overlays/default.nix
+++ overlays/default.nix
@@ -38,20 +38,17 @@
     patched = {
       # Use latest lima, it makes faster build especially patching: https://github.com/NixOS/nixpkgs/pull/415093
       lima = prev.unstable.lima.overrideAttrs (
-        finalAttrs: previousAttrs:
-        if prev.stdenv.hostPlatform.isLinux then
-          {
-            patches = [
-              (prev.fetchpatch {
-                # https://github.com/kachick/lima/pull/1
-                name = "lima-suppress-gssapi-warning.patch";
-                url = "https://patch-diff.githubusercontent.com/raw/kachick/lima/pull/1.patch";
-                hash = "sha256-QTEYorN+nj66WMlMz+hsoZUWPnlGPDCw0VSsqsiayls=";
-              })
-            ];
-          }
-        else
-          { }
+        finalAttrs: previousAttrs: {
+          # This patch is needed on Linux. However enables on macOS too for testing.
+          patches = [
+            (prev.fetchpatch {
+              # https://github.com/lima-vm/lima/pull/3637
+              name = "lima-suppress-gssapi-warning.patch";
+              url = "https://github.com/lima-vm/lima/pull/3637.patch?full_index=1";
+              hash = "sha256-8RM4bR46qPdkis4mqoScO20seQrOHz/22L3b0+ovYeM=";
+            })
+          ];
+        }
       );
 
       # TODO: Remove after merging https://github.com/NixOS/nixpkgs/pull/301440
