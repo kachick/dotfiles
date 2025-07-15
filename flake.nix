ed3d80c4a83e1943172effa22b38ede369180a71
Kenichi Kamiya <kachick1@gmail.com>
2025-07-15 14:14:27 +0900

    Install zizmor in devshell

--- flake.nix
+++ flake.nix
@@ -127,6 +127,7 @@
                   hydra-check # Background and how to use: https://github.com/kachick/dotfiles/pull/909#issuecomment-2453389909
                   gitleaks
                   dprint
+                  zizmor
                 ])
                 ++ (with pkgs.my; [ nix-hash-url ])
                 ++ [
