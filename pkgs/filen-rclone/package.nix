420a3b38c4104c56dbeb7bdb1523f5e787e68844
Kenichi Kamiya <kachick1@gmail.com>
2025-09-09 04:31:20 +0900

    Add notes about dangers about filen-rclone

--- pkgs/filen-rclone/package.nix
+++ pkgs/filen-rclone/package.nix
@@ -3,9 +3,15 @@
   fetchFromGitHub,
   ...
 }:
+# Don't install both rclone and filen-rclone together. They have same bin names.
+# And I don't prefer filen-rclone for other purpose for the stability.
+# Use latest official rclone for basic use-case
+# So I'll only use filen-rclone through `nix run` for now.
 rclone.overrideAttrs (
   finalAttrs: previousAttrs: {
     pname = "filen-rclone";
+    # MUST NOT use 1.70.0-filen.11 or older
+    # They lacks the crucial patch https://github.com/FilenCloudDienste/filen-sdk-go/commit/cd9f4e00f07adf815d17ff1125751c943160f9f3
     version = "1.70.0-filen.12";
 
     src = fetchFromGitHub {
