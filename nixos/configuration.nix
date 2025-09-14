a4a57b38a4ff0f2754164c9215bb75e20b69ad69
Kenichi Kamiya <kachick1@gmail.com>
2025-09-14 16:05:51 +0900

    Drop avahi and only use systemd-resolved for mDNS (#1266)
    Follow more 1fc4d28

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -166,31 +166,46 @@
   # - https://github.com/NixOS/nixpkgs/issues/118628
   # - https://github.com/NixOS/nixpkgs/issues/412777
   # - https://github.com/NixOS/nixpkgs/issues/291108
+  # I don't know how to use both in NixOS likely https://wiki.archlinux.org/index.php?title=CUPS&diff=prev&oldid=806890
   services.avahi = {
     # Enable auto detect for wireless printers. CUPS does not support systemd-resolved
     # - https://github.com/apple/cups/issues/5452
     # - https://github.com/OpenPrinting/libcups/issues/81
-    enable = true; # If enabled, you should care the conflict with systemd-resolved
+    enable = false; # If enabled, you should care the conflict with systemd-resolved
 
-    # Make sure disabling mDNS in avahi to avoid conflict with systemd-resolved
+    # I don't know how to realize enabling DNS-SD but disable mDNS: https://wiki.archlinux.org/index.php?title=CUPS&diff=prev&oldid=806890
+    # Check the log with `journalctl -u systemd-resolved -u avahi-daemon -r`
     # I prefer systemd-resolved for mDNS use, because of enabling on Avahi makes much flaky resolutions
     # You can test it with: `avahi-resolve-host-name hostname.local` if enabled
-    nssmdns4 = false;
-    nssmdns6 = false;
   };
 
   ## systemd-resolved (Modern, not supported by CUPS)
+  # - Check the behavior with `resolvectl status`
   # - https://github.com/NixOS/nixpkgs/blob/nixos-25.05/nixos/modules/system/boot/resolved.nix
   # - https://wiki.archlinux.org/title/Systemd-resolved
   # Disabled by default. But ensures to disable MulticastDNS
   services.resolved = {
     enable = true;
+    llmnr = "false";
 
     # Enable mDNS(hostname.local). Consider to avoid conflict with Avahi
-    # My motivation come from: https://wiki.archlinux.org/index.php?title=CUPS&diff=prev&oldid=806890
     extraConfig = ''
       MulticastDNS=true
     '';
+  };
+
+  # Avahi module has openFirewall, but resolved module does not have it
+  networking.firewall.allowedUDPPorts = [ 5353 ];
+
+  # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/nixos/modules/services/networking/networkmanager.nix
+  networking.networkmanager = {
+    enable = true;
+
+    dns = "systemd-resolved";
+    connectionConfig."connection.mdns" = 2;
+
+    # TIPS: If you are debugging, dmesg with ctime/iso will display incorrect timestamp
+    # Then `journalctl --dmesg --output=short-iso --since='1 hour ago' --follow` might be useful
   };
 
   # Some programs need SUID wrappers, can be configured further or are
