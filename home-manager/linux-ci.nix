fd654b658bce718e11fc961e464a88e2867671e1
Kenichi Kamiya <kachick1@gmail.com>
2025-09-27 16:19:52 +0900

    Revert "Attempt to extract building patched mozc in separate workflow"
    This reverts commit 47ecde348014176d0bd5a87374084ec52d24ca85.

--- home-manager/linux-ci.nix
+++ home-manager/linux-ci.nix
@@ -32,6 +32,9 @@
         # pkgs.patched.lima # Enable when patched
       ]
       # These packages are override original pname instead of adding new namespace. So required to build the binary cache here. I'm unsure how to run these in ci-nix
+      ++ [
+        pkgs.gnome-keyring
+      ]
       ++ (with pkgs.ibus-engines; [ mozc ]);
   };
 }
