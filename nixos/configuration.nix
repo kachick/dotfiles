3312b0fe419908c79505b2d8c54788eb08d61258
Kenichi Kamiya <kachick1@gmail.com>
2025-10-07 12:32:14 +0900

    Add numtide.cachix.org as a trusted substituters

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -34,16 +34,17 @@
     always-allow-substitutes = true;
 
     trusted-substituters = [
+      "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
       "kachick-dotfiles.cachix.org-1:XhiP3JOkqNFGludaN+/send30shcrn1UMDeRL9XttkI="
     ];
 
     trusted-public-keys = [
-      # https://nix-community.org/cache/
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-
-      # GH-1235
-      "kachick-dotfiles.cachix.org-1:XhiP3JOkqNFGludaN+/send30shcrn1UMDeRL9XttkI="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" # https://nix-community.org/cache/
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "kachick-dotfiles.cachix.org-1:XhiP3JOkqNFGludaN+/send30shcrn1UMDeRL9XttkI=" # GH-1235
     ];
 
     accept-flake-config = true;
