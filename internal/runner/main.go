9018857f5d72321ad92a00aac443138ebee81301
Kenichi Kamiya <kachick1@gmail.com>
2025-10-10 04:32:56 +0900

    Refactor by WaitGroup.Go

--- internal/runner/main.go
+++ internal/runner/main.go
@@ -19,15 +19,13 @@ type Commands []Cmd
 func (cmds Commands) ParallelRun() {
 	wg := &sync.WaitGroup{}
 	for _, cmd := range cmds {
-		wg.Add(1)
-		go func(cmd Cmd) {
-			defer wg.Done()
+		wg.Go(func() {
 			output, err := exec.Command(cmd.Path, cmd.Args...).CombinedOutput()
 			log.Printf("%s %s\n%s\n", cmd.Path, runewidth.Truncate(strings.Join(cmd.Args, " "), 60, "..."), string(output))
 			if err != nil {
 				log.Fatalln(err)
 			}
-		}(cmd)
+		})
 	}
 	wg.Wait()
 }
