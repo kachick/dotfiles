27c5b46fb590a6bd827182b2d0e2cc1f47e29a67
Kenichi Kamiya <kachick1@gmail.com>
2025-10-28 11:37:33 +0900

    Add comment about bash integrations

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -12,7 +12,7 @@
   programs.fzf.enableBashIntegration = false; # GH-1192: Don't enable fzf integrations, it makes shell startup slower. Load only key-bindings if required.
   programs.television.enableBashIntegration = false; # Conflict with fzf by default
   programs.zellij.enableBashIntegration = false; # Intentionally disabled for keeping stable bash
-  programs.atuin.enableBashIntegration = false; # Because of atuin depends on ble.sh
+  programs.atuin.enableBashIntegration = false; # Because of atuin depends on ble.sh. See also https://github.com/atuinsh/atuin/issues/1798
 
   # Used only in bash - https://unix.stackexchange.com/a/689403
   # https://github.com/nix-community/home-manager/blob/release-24.11/modules/programs/readline.nix
