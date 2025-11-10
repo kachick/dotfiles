6bd1573e200e5063b2ba0ae1c403dc1513c4d236
Kenichi Kamiya <kachick1@gmail.com>
2025-11-10 16:54:16 +0900

    yaneuraou: Backport merged changes from nixpkgs
    https://github.com/NixOS/nixpkgs/commit/fcacfd0484bbc9e7363c5c4d62fe6c13f42159c5
    
    Available since 638c56e39858c13e9c7ab55d313a980a7110be33

--- overlays/default.nix
+++ overlays/default.nix
@@ -111,21 +111,6 @@
           };
         }
       );
-
-      yaneuraou = prev.unstable.yaneuraou.overrideAttrs (
-        finalAttrs: previousAttrs: {
-          # Require https://github.com/yaneurao/YaneuraOu/commit/33dce0bfa363f63d99977c29b3d6ab40ff896138
-          # See https://github.com/yaneurao/YaneuraOu/issues/304#issuecomment-3405888952 for detail
-          version = "9.01";
-
-          src = prev.fetchFromGitHub {
-            owner = "yaneurao";
-            repo = "YaneuraOu";
-            tag = "v${finalAttrs.version}git";
-            hash = "sha256-uhr3jS+ttN5pF1zZpHq2xWy3sdMV19eRUhuj2uPspak=";
-          };
-        }
-      );
     };
   })
 ]
