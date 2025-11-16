91f7012785ff90cb1ba317131fa533e2f8b2f2c8
Kenichi Kamiya <kachick1@gmail.com>
2025-11-17 03:08:54 +0900

    Update comments for unstable Nix packages

--- home-manager/linux-ci.nix
+++ home-manager/linux-ci.nix
@@ -10,26 +10,26 @@
 # Providing since GH-1085. It migtht be superseded by GH-642.
 #
 # Limitation: Don't add unfree packages such as cloudflare-warp, vscode
+#
+# Omit if:
+#   - Which is build on ci-nix workflow: typos, dprint
+#   - Which is NixOS core: gnome-*
 
 {
   home = {
     packages =
-      (with pkgs.unstable; [
-        # Disabling zed-editor because of it is most flaky in unstable channels.
-        # If you want to use newer versions, consider to use upstream providing binary cache
-        # See https://github.com/zed-industries/zed/issues/19937 for detail
-        ## zed-editor
+      # (with pkgs.unstable; [
+      #   # Disabling zed-editor because it was very flaky in unstable channel.
+      #   # By the way, upstream's cache https://github.com/zed-industries/zed/issues/19937 is for nightly use.
+      #   # It does not ensure providing cache for tagged versions.
+      #   ## zed-editor
+      # ]) ++
 
-        typos-lsp
-        dprint
-        # gnome-boxes # Omitting. I believe it is a core package of NixOS.
-      ])
       # Test builds and push the binary cache from CI
       # Consider to use ci-nix instead
       # Don't use `with` to keep indentation even if empty list
-      ++ [
+      [
         # pkgs.patched.pname
-        # pkgs.patched.lima # Enable when patched
       ]
       # These packages are override original pname instead of adding new namespace. So required to build the binary cache here. I'm unsure how to run these in ci-nix
       # ++ [
