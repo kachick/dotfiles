2d43264e269c2563755c6ce9feba6d7606b6fdbb
Kenichi Kamiya <kachick1@gmail.com>
2025-11-26 00:06:33 +0900

    Follow restructured programs.git

--- home-manager/genericUser.nix
+++ home-manager/genericUser.nix
@@ -6,11 +6,13 @@
   home.username = lib.mkDefault "user";
 
   programs.git = {
-    # https://datatracker.ietf.org/doc/html/rfc6761
-    userEmail = "foobar@example.com";
-    userName = "Foo Bar";
+    settings = {
+      user = {
+        # https://datatracker.ietf.org/doc/html/rfc6761
+        email = "foobar@example.com";
+        name = "Foo Bar";
+      };
 
-    extraConfig = {
       ghq.user = "github";
     };
   };
