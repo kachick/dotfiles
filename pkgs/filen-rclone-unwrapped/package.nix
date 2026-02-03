ee6b8f6f46e4698de6c47c967cf09326d9f99b9d
Kenichi Kamiya <kachick1@gmail.com>
2026-02-03 11:12:52 +0900

    filen-rclone: 1.70.0-filen.15 -> 1.73.0 with Filen patch (#1421)

--- pkgs/filen-rclone-unwrapped/package.nix
+++ pkgs/filen-rclone-unwrapped/package.nix
@@ -1,28 +1,41 @@
 {
   pkgs,
   fetchFromGitHub,
+  fetchpatch2,
   ...
 }:
 
-# Don't install both rclone and this forked rclone together. They have same bin names with different logics. (And the different config schema!)
+# Don't install both rclone and this forked rclone together. They had same bin names with different logics. (And the different config schema!)
 # Even if changed the priority, it makes confusing.
 # And I don't prefer filen-rclone for other purpose for the stability.
-# Prefer official rclone for non filen providers
-pkgs.rclone.overrideAttrs (
+# Prefer official rclone package for non filen providers
+pkgs.unstable.rclone.overrideAttrs (
   finalAttrs: previousAttrs: {
     pname = "filen-rclone-unwrapped";
-    # MUST NOT use 1.70.0-filen.11 or older
+    # MUST NOT use FilenCloudDienste/filen-rclone's 1.70.0-filen.11 or older
     # They lacks the crucial patch https://github.com/FilenCloudDienste/filen-sdk-go/commit/cd9f4e00f07adf815d17ff1125751c943160f9f3
-    version = "1.70.0-filen.15";
+    version = "1.73.0";
 
+    # Since 1.73.0, official rclone merged filen-rclone
+    # It is frequently updated rather than FilenCloudDienste/filen-rclone
+    # However it is not yet mergd in nixpkgs: https://github.com/NixOS/nixpkgs/pull/485515
     src = fetchFromGitHub {
-      owner = "FilenCloudDienste";
-      repo = "filen-rclone";
+      owner = "rclone";
+      repo = "rclone";
       tag = "v${finalAttrs.version}";
-      hash = "sha256-1EqtjBU0GV3+zhOYBQGtfFNox0xI+6uuMITWxz/wlyM=";
+      hash = "sha256-g/ofD/KsUOXVTOveHKddPN9PP5bx7HWFPct1IhJDZYE=";
     };
 
-    vendorHash = "sha256-GVbec1V8hejhNekCPJ808i23qkDZOvNI4xneEFVZKTI=";
+    patches = [
+      # Unmerged patch for: https://github.com/FilenCloudDienste/filen-rclone/issues/5
+      (fetchpatch2 {
+        name = "fix-potential-panic-in-case-of-error-during-upload.patch";
+        url = "https://patch-diff.githubusercontent.com/raw/rclone/rclone/pull/9140.patch?full_index=1";
+        hash = "sha256-7hz6C+EExdBctHD/8X9Fn8MBeERfgVEtORBb1Zr7DTg=";
+      })
+    ];
+
+    vendorHash = "sha256-LomeLlk0d/HTL0NKmbd083u7BHsy4FmAah9IzvmtO2s=";
 
     # nixpkgs definition is still using rec, so I should override at here until using finalAttrs
     # https://github.com/NixOS/nixpkgs/blob/480f847ddc3ccab43a76f31f713653b5ad79ba3f/pkgs/by-name/rc/rclone/package.nix#L18
