5ca294d4a7b14477bd55927cd39c896e02052531
Kenichi Kamiya <kachick1@gmail.com>
2026-02-13 20:27:28 +0900

    rclone-mount: improve idempotency with mountpoint check

--- pkgs/rclone-mount/package.nix
+++ pkgs/rclone-mount/package.nix
@@ -4,11 +4,13 @@ pkgs.writeShellApplication rec {
   text = builtins.readFile ./${name}.bash;
   runtimeInputs = with pkgs; [
     coreutils # `mktemp`
+    util-linux # `mountpoint` (Not available on Darwin)
     patched.rclone
   ];
   meta = {
     description = "Mount rclone to my usual directory";
     # Didn't work on Darwin. It might work when disabling --daemon or replacing the default NFS with FUSE. However, I'm very tired to consider Darwin.
+    # If supporting Darwin in the future, keep in mind that `mountpoint` command is not available.
     platforms = lib.platforms.linux;
   };
 }
--- pkgs/rclone-mount/rclone-mount.bash
+++ pkgs/rclone-mount/rclone-mount.bash
@@ -1,11 +1,18 @@
 rclone-mount() {
 	local -r remote="$1" # e.g `foo:`
-	# TODO: Return the path if already mounted the remote
 	local -r mount_to="$XDG_DATA_HOME/mnt/rclone/${remote%:}"
+
+	if mountpoint -q "$mount_to"; then
+		echo 'Already mounted:' >&2
+		echo "$mount_to"
+		return 0
+	fi
+
 	mkdir --parent "$mount_to"
 	# Use `kill``, not `kill -9`. If exists the garbages in `mount --types fuse.rclone`, `fusermount -u THE_PATH`
 	# See https://www.reddit.com/r/rclone/comments/nh576p/how_to_stop_an_rclone_daemon/
 	rclone mount "$remote" "$mount_to" --vfs-cache-mode writes --daemon
+	echo 'Mmounted to:' >&2
 	echo "$mount_to"
 }
 
