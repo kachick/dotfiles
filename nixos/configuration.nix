bd96a4e0dff7b999b967481f063804c6dfce25bc
Kenichi Kamiya <kachick1@gmail.com>
2026-03-01 17:01:57 +0900

    Update extra-trusted-substituters and the public-keys
    - Add cache.garnix.io
    - Update numtide entry
    - Fix URL of this repository, I think it worked via flake config
    - Add selfup.cachix.org for now

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -33,16 +33,21 @@
     # See https://github.com/NixOS/nix/pull/8047 for background
     always-allow-substitutes = true;
 
+    # Remember https://garnix.io/blog/stop-trusting-nix-caches/ if you adding new entry
     extra-trusted-substituters = [
-      "https://nix-community.cachix.org"
-      "https://numtide.cachix.org"
-      "kachick-dotfiles.cachix.org-1:XhiP3JOkqNFGludaN+/send30shcrn1UMDeRL9XttkI="
+      "https://nix-community.cachix.org" # https://nix-community.org/cache/
+      "https://cache.garnix.io"
+      "https://cache.numtide.com" # Replaced from https://numtide.cachix.org: https://github.com/numtide/treefmt/pull/655
+      "https://selfup.cachix.org" # GH-1235
+      "https://kachick-dotfiles.cachix.org"
     ];
 
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" # https://nix-community.org/cache/
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "kachick-dotfiles.cachix.org-1:XhiP3JOkqNFGludaN+/send30shcrn1UMDeRL9XttkI=" # GH-1235
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      "selfup.cachix.org-1:eY2eEd955BmRI7SultbRIV81vApqpJixunkV3XlXuT8="
+      "kachick-dotfiles.cachix.org-1:XhiP3JOkqNFGludaN+/send30shcrn1UMDeRL9XttkI="
     ];
 
     accept-flake-config = true;
