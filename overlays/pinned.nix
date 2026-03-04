0666e6ca9226671ca229631cf5f9465d0cf90e37
Kenichi Kamiya <kachick1@gmail.com>
2026-03-05 00:13:19 +0900

    Update comment about zed-editor package
    The last trouble seems not related to the workflow, it just come from Hydra build

--- overlays/pinned.nix
+++ overlays/pinned.nix
@@ -6,19 +6,16 @@ in
 {
   # Pinned or external packages that we want to build and cache in CI.
   #
-  # Why do I need special handling for zed-editor?:
-  #   See GH-1085, GH-1134 and GH-1402
-  #   zed-editor package was very flaky in hydra between both stable and unstable channels.
-  #   The authors send and package maintainers merge updating PRs immediately without attempting build on any platforms.
-  #   Since I can't trust stable channel, unstable channel is a bit better to fix it faster.
-  #
   # Limitation: Don't add unfree packages such as cloudflare-warp, vscode
   pinned = {
     # Pinning kanata-tray from the flake input
     # It seems kanata-tray flake provides 'kanata-tray' and 'default' (which is kanata-tray)
     kanata-tray = kanata-tray.packages.${system}.default;
 
-    # Pinned from nixpkgs-unstable
+    # See GH-1085, GH-1134, and GH-1402
+    # The zed-editor package has been very flaky in Hydra: https://discourse.nixos.org/t/why-is-zed-editor-not-cached/60452/4
+    # Since I can't trust stable channels, I use the unstable channel to resolve package-level issues faster,
+    # and I build and cache it myself.
     inherit (_prev.unstable) zed-editor;
 
     # Expose the patched mozc for CI building
