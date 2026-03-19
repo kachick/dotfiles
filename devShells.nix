1f06c8d8274c74403c980bd35585e9c8193d17a3
Kenichi Kamiya <kachick1@gmail.com>
2026-03-19 14:15:27 +0900

    Unify how to avoid nix develop by using direnv on GHA (#1508)
    Extracting CI inputs from devShell inputs in 6551dce284da04f794a87f4fc6e51d9c59386414 seemed like an elegant solution at first, but it increased the maintenance overhead of managing their differences.
    For better cohesion, they should be defined together.
    
    ---
    
    * Avoid nix develop on GHA
    
    * Revert "Factor out CI tools and enable lightweight profile installation (#1494)"
    
    This reverts commit 6551dce284da04f794a87f4fc6e51d9c59386414.
    
    * Unify devshell setup with direnv on the GHA

--- devShells.nix
+++ devShells.nix
@@ -16,20 +16,46 @@ in
       TYPOS_LSP_PATH = pkgs.lib.getExe typos-lsp; # For vscode typos extension
     };
 
-    buildInputs = [
-      pkgs.local.ci
-      # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-      pkgs.bashInteractive
-    ]
-    ++ (pkgs.lib.optionals pkgs.stdenv.isLinux (
+    buildInputs =
       (with pkgs; [
-        nixd
-        nurl
-        nix-update
+        # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+        bashInteractive
+        go-task
       ])
-      ++ [
-        typos-lsp # For zed-editor typos extension
-      ]
-    ));
+      ++ (pkgs.lib.optionals pkgs.stdenv.isLinux (
+        (with pkgs; [
+          nixd
+          nixf-diagnose
+          nurl
+          nix-update
+
+          shellcheck
+          shfmt
+
+          # We don't need to consider about treefmt1 https://github.com/NixOS/nixpkgs/pull/387745
+          treefmt
+
+          trivy
+
+          desktop-file-utils # `desktop-file-validate` as a linter
+          kanata # Enable on devshell for using the --check as a linter
+        ])
+        ++ (with pkgs.unstable; [
+          nixfmt # Finally used this package name again. See https://github.com/NixOS/nixpkgs/pull/425068 for detail
+          gitleaks
+          typos
+          lychee
+          dprint
+          zizmor
+          rumdl # Available since https://github.com/NixOS/nixpkgs/pull/446292
+          go_1_26
+        ])
+        ++ (with pkgs.local; [
+          nix-hash-url
+        ])
+        ++ [
+          typos-lsp # For zed-editor typos extension
+        ]
+      ));
   };
 }
