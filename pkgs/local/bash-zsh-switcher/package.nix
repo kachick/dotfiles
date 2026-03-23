a1ac840fa9a9521ff80243720e3038dfbb682ac8
Kenichi Kamiya <kachick1@gmail.com>
2026-03-23 21:06:19 +0900

    Remove redundant comments in bash-zsh-switcher package

--- pkgs/local/bash-zsh-switcher/package.nix
+++ pkgs/local/bash-zsh-switcher/package.nix
@@ -5,7 +5,6 @@
   ...
 }:
 
-# This script is strictly for Bash, intended to be used in .bashrc (initExtra).
 # Use replaceVars to enable basic shellscript helpers such as shfmt, shellcheck and syntax highlighters
 #
 # Note: Using `replaceVars` directly avoids Import From Derivation (IFD) which was caused by the previous `builtins.readFile` + `writeText` wrapper.
