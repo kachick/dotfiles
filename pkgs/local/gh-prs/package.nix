c7ba1c77c477b75a814617e887c006ec0bd38d03
Kenichi Kamiya <kachick1@gmail.com>
2026-05-25 22:19:26 +0900

    Drop wslu
    https://github.com/NixOS/nixpkgs/pull/508167/changes/baa4321760ad72c0f779d918d598a079814d2b86

--- pkgs/local/gh-prs/package.nix
+++ pkgs/local/gh-prs/package.nix
@@ -2,17 +2,12 @@
 pkgs.writeShellApplication rec {
   name = "gh-prs";
   text = builtins.readFile ./${name}.bash;
-  runtimeInputs =
-    with pkgs;
-    [
-      coreutils
-      fzf
-      gh
-      local.wait-and-squashmerge
-    ]
-    ++ (lib.optionals stdenv.isLinux [
-      wslu # WSL helpers like `wslview`. It is used in open browser features in gh command
-    ]);
+  runtimeInputs = with pkgs; [
+    coreutils
+    fzf
+    gh
+    local.wait-and-squashmerge
+  ];
   derivationArgs = {
     # Required in https://github.com/nix-community/home-manager/blob/346973b338365240090eded0de62f7edce4ce3d1/modules/programs/gh.nix#L160
     pname = name;
--- pkgs/local/git-log-fzf/package.nix
+++ pkgs/local/git-log-fzf/package.nix
@@ -2,21 +2,16 @@
 pkgs.writeShellApplication rec {
   name = "git-log-fzf";
   text = builtins.readFile ./${name}.bash;
-  runtimeInputs =
-    with pkgs;
-    [
-      fzf
-      coreutils
-      gitMinimal
-      gh
-      colorized-logs
-      bat
-      riffdiff
-      local.git-log-simple
-    ]
-    ++ (lib.optionals stdenv.isLinux [
-      wslu # WSL helpers like `wslview`. It is used in open browser features in gh command
-    ]);
+  runtimeInputs = with pkgs; [
+    fzf
+    coreutils
+    gitMinimal
+    gh
+    colorized-logs
+    bat
+    riffdiff
+    local.git-log-simple
+  ];
   meta = {
     description = "Faster git log searcher for large repositories(e.g NixOS/nixpkgs)";
   };
