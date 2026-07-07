3ab8a59e34004f1ec2d9aa78df32b88ea57002c5
Kenichi Kamiya <kachick1@gmail.com>
2026-07-08 02:23:19 +0900

    Move zellij config into terminals

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -201,25 +201,6 @@ in
     };
   };
 
-  # https://github.com/nix-community/home-manager/blob/release-26.05/modules/programs/zellij.nix
-  programs.zellij = {
-    enable = true;
-
-    # Don't use settings, nix and KDL is much unfit: https://github.com/NixOS/nixpkgs/issues/198655#issuecomment-1453525659
-  };
-  xdg.configFile."zellij" = {
-    source = ../config/zellij;
-    recursive = true;
-  };
-  xdg.configFile."zellij/simplified-ui.kdl" = {
-    text = builtins.readFile ../config/zellij/config.kdl + ''
-      // Use a simplified UI without special fonts (arrow glyphs)
-      // This is necessary on Linux VT to avoid Tofu
-      // Or you can run zellij with `zellij options --simplified-ui true`
-      simplified_ui true
-    '';
-  };
-
   # TODO: Switch to raw config file and shared in Windows
   programs.ripgrep = {
     enable = true;
--- home-manager/terminals.nix
+++ home-manager/terminals.nix
@@ -4,6 +4,13 @@
 }:
 
 {
+  # https://github.com/nix-community/home-manager/blob/release-26.05/modules/programs/zellij.nix
+  programs.zellij = {
+    enable = true;
+
+    # Don't use settings, nix and KDL is much unfit: https://github.com/NixOS/nixpkgs/issues/198655#issuecomment-1453525659
+  };
+
   xdg = {
     configFile = {
       # Make sure to enable the NixOS module with empty file. See xdg.terminal-exec for details
@@ -15,6 +22,20 @@
       "alacritty/themes" = {
         source = ../config/alacritty/themes;
         recursive = true;
+      };
+
+      zellij = {
+        source = ../config/zellij;
+        recursive = true;
+      };
+
+      "zellij/simplified-ui.kdl" = {
+        text = builtins.readFile ../config/zellij/config.kdl + ''
+          // Use a simplified UI without special fonts (arrow glyphs)
+          // This is necessary on Linux VT to avoid Tofu
+          // Or you can run zellij with `zellij options --simplified-ui true`
+          simplified_ui true
+        '';
       };
     }
     // (mkWritableConfig.xdg "ghostty/config" ../config/ghostty/config { });
